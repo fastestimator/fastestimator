1270a3cda38fa1038e19f2e1421f0b6e79e56fe8
Xiaomeng Dong <Xiaomeng.dong@ge..com>
2019-03-28 15:26:06 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fastestimator',
+  packages = ['fastestimator'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Edittool',
+  author_email = 'noreply@google.com',
+  url = 'https://github.com/fastestimator/fastestimator', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/fastestimator/fastestimator/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['fastestimator'], # arbitrary keywords
+  classifiers = [],
+)
