b53f2905196d983de5fbef245cf279bbde18be8b
JP Hong <junpyo.hong@ge.com>
2019-06-18 12:02:00 -0700

    resolving conflict

--- test/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
-echo $WROKSPACE
-if [ ! -d "venv"  ]; then
-    virtualenv venv
-fi
-. venv/bin/activate
-
-pip3 install pytest numpy nibabel pydicom horovod
-pip3 install -e .
