9dd23f66a1a052271caa3d9ab4b4b2c8cd69b6cd
Vivek Soni <vivek.soni@ge.com>
2019-06-20 17:44:11 -0700

    workspace dir fixed

--- test/install_dependencies.sh
+++ test/install_dependencies.sh
@@ -1,5 +1,5 @@
-JENKINS_DIR=$WORKSPACE
-PATH=$JENKINS_DIR/../venv/bin:/usr/local/bin:$PATH
+JENKINS_DIR=$WORKSPACE/..
+PATH=$JENKINS_DIR/venv/bin:/usr/local/bin:$PATH
 echo $JENKINS_DIR
 echo $PATH
 if [ ! -d "$JENKINS_DIR/venv"  ]; then
