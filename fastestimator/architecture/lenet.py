4166722d3845e6379ed16ec51329381e8e684bc5
Potter, Michael (GE Digital) <michael.potter@ge.com>
2019-07-09 12:28:09 -0700

    remove time axis from training (+1 squashed commit) Squashed commits: [c64ea68] time dimension (+8 squashed commits) Squashed commits: [61d4f71] cleanup [69f3fc4] improve mixup efficiency [56c3cfb] cleanup [7387f58] disable recursive search by default for log visualization [3bf7a77] remove dataset hack [5efb29b] back to relu [c4f63f1] rule around 2k example/sec, swish around 1.3k [475167c] first pass: 95 examples / sec

--- fastestimator/architecture/lenet.py
+++ fastestimator/architecture/lenet.py
@@ -1,7 +1,8 @@
-from tensorflow.keras import layers, models
+from tensorflow.python.keras import layers, Sequential
 
-def LeNet(input_shape=(28,28,1), classes=10):
-    model = models.Sequential()
+
+def LeNet(input_shape=(28, 28, 1), classes=10):
+    model = Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
--- showcase/image_classification/lenet_mnist.py
+++ showcase/image_classification/lenet_mnist.py
@@ -27,7 +27,8 @@ class Network:
         loss = self.loss(batch["y"], predictions)
         return predictions, loss
 
-def get_estimator(epochs=2, batch_size=32, optimizer="adam"):
+
+def get_estimator(epochs=2, batch_size=32):
 
     (x_train, y_train), (x_eval, y_eval) = tf.keras.datasets.mnist.load_data()
     x_train = np.expand_dims(x_train, -1)
