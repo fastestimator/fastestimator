d865e9e6539020f48199ff660353293c1a089cba
Hsi-Ming Chang <hsi-ming.chang@ge.com>
2019-07-25 11:37:57 -0700

    Add image segmentation example

--- fastestimator/estimator/trace.py
+++ fastestimator/estimator/trace.py
@@ -243,11 +243,11 @@ class Dice(Trace):
         self.dice = None
 
     def on_epoch_begin(self, mode, logs):
-        if mode in ["train", "eval"]:
+        if mode in ["eval"]:
             self.dice = None
 
     def on_batch_end(self, mode, logs):
-        if mode in ["train", "eval"]:
+        if mode in ["eval"]:
             groundtruth_label = np.array(logs["batch"][self.y_true_key])
             if groundtruth_label.shape[-1] > 1 and groundtruth_label.ndim > 1:
                 groundtruth_label = np.argmax(groundtruth_label, axis=-1)
@@ -257,8 +257,6 @@ class Dice(Trace):
                 prediction_score = np.array(prediction[self.y_pred_key])
             else:
                 prediction_score = np.array(prediction)
-
-            print('{} max pred: {}'.format(mode, np.amax(prediction_score)))
             prediction_label = (prediction_score >= self.threshold).astype(np.int)
 
             intersection = np.sum(groundtruth_label * prediction_label, axis=(1, 2, 3))
@@ -270,6 +268,6 @@ class Dice(Trace):
                 self.dice = np.append(self.dice, dice, axis=0)
 
     def on_epoch_end(self, mode, logs):
-        if mode in ["train", "eval"]:
+        if mode in ["eval"]:
             return np.mean(self.dice)
         return None
---
+++ showcase/image_segmentation/unet_cub200.py
@@ -0,0 +1,94 @@
+"""U-Net bird segmentation example.
+"""
+import os
+import tempfile
+
+import tensorflow as tf
+
+from fastestimator.architecture.unet import unet
+from fastestimator.dataset import cub200
+from fastestimator.estimator.estimator import Estimator
+from fastestimator.estimator.trace import Dice
+from fastestimator.pipeline.dynamic.preprocess import AbstractPreprocessing, ImageReader, MatReader, Resize
+from fastestimator.pipeline.pipeline import Pipeline
+from fastestimator.pipeline.static.preprocess import Minmax, Reshape
+
+DATA_SAVE_PATH = os.path.join(tempfile.gettempdir(), 'CUB200')
+
+# Download CUB200 dataset.
+cub200.load_data(DATA_SAVE_PATH)
+
+class Network:
+    """Load U-Net and define train and eval ops.
+    """
+    def __init__(self):
+        self.model = unet("image", "annotation")
+        self.optimizer = tf.optimizers.Adam(learning_rate=0.0001)
+        self.loss = tf.losses.BinaryCrossentropy()
+
+    def train_op(self, batch):
+        """Training loop.
+
+        Args:
+            batch (`Tensor`): Batch data for training.
+
+        Returns:
+            `Tensor`: Network output and loss value.
+        """
+        with tf.GradientTape() as tape:
+            predictions = self.model(batch["image"])
+            loss = self.loss(batch["annotation"], predictions)
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        return predictions, loss
+
+    def eval_op(self, batch):
+        """Evaluation loop.
+
+        Args:
+            batch (`Tensor`): Batch data for evaluation.
+
+        Returns:
+            `Tensor`: Network output and loss value.
+        """
+        predictions = self.model(batch["image"], training=False)
+        loss = self.loss(batch["annotation"], predictions)
+        return predictions, loss
+
+
+class SelectKey(AbstractPreprocessing):
+    """Select specific dict value.
+    """
+    def transform(self, data, feature=None):
+        data = data['seg']
+        return data
+
+
+def get_estimator():
+    """Generate FastEstimator estimator.
+
+    Returns:
+        Estimator object.
+    """
+    pipeline = Pipeline(
+        batch_size=64,
+        feature_name=["image", "annotation"],
+        train_data=os.path.join(DATA_SAVE_PATH, 'cub200.csv'),
+        validation_data=0.2,
+        transform_dataset=[
+            [ImageReader(),
+             Resize((128, 128), keep_ratio=True)],
+            [MatReader(), SelectKey(), Resize((128, 128), keep_ratio=True)]
+        ],
+        transform_train=[[Reshape((128, 128, 3)), Minmax()], [Reshape((128, 128, 1))]])
+
+    traces = [Dice(y_true_key="annotation")]
+
+    estimator = Estimator(network=Network(),
+                          pipeline=pipeline,
+                          epochs=2000,
+                          steps_per_epoch=10,
+                          validation_steps=1,
+                          log_steps=10,
+                          traces=traces)
+    return estimator
