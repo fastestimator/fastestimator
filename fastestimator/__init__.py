81802193082025172309d0f2f680616ee32d0e30
Hsi-Ming Chang <hsi-ming.chang@ge.com>
2019-08-06 17:42:13 -0700

    Fix header

--- fastestimator/__init__.py
+++ fastestimator/__init__.py
@@ -1,16 +1,16 @@
-# Copyright 2019 The FastEstimator Authors import  All Rights Reserved import
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2 import 0 (the "License");
-# you may not use this file except in compliance with the License import
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www import apache import org/licenses/LICENSE-2 import 0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied import
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License import
+# limitations under the License.
 # ==============================================================================
 from fastestimator import architecture
 from fastestimator import dataset
