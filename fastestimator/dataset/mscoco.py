0124f0bbef2a43af426c5274e4d652083d1f697c
JP Hong <junpyo.hong@ge.com>
2019-08-12 17:35:17 -0700

    Style Transfer (+1 squashed commit) Squashed commits: [2a35bb1] Edited Readme

---
+++ fastestimator/dataset/mscoco.py
@@ -0,0 +1,58 @@
+import os
+import tempfile
+import zipfile
+import wget
+
+import pandas as pd
+
+from glob import glob
+
+NUM_IMG_FILES = 82783
+
+
+def _download_and_extract(url, filename, path):
+    print("Downloading data to {}".format(path))
+    wget.download(url, path)
+    with zipfile.ZipFile(filename, 'r') as zip_file:
+        print("Extracting {}".format(filename))
+        zip_file.extractall(path)
+
+
+def _create_csv(data_path):
+    filenames = glob(os.path.join(data_path, '*.jpg'))
+    csv = None
+    if len(filenames) == NUM_IMG_FILES:
+        csv = pd.DataFrame()
+        rel_filenames = [f.replace(data_path, '.') for f in filenames]
+        csv["image"] = rel_filenames
+    else:
+        print("One or more images are missing")
+    return csv
+
+
+def load_data(path=None):
+    url = {"train": "http://images.cocodataset.org/zips/train2014.zip"}
+    path = os.path.abspath(path)
+    if path is None:
+        path = os.path.join(tempfile.gettempdir(), "FE_MSCOCO2014")
+
+    os.makedirs(path, exist_ok=True)
+    train_csv_path = os.path.join(path, "coco_train.csv")
+    train_data_dir = os.path.join(path, "train2014")
+    if not (os.path.exists(train_data_dir)):
+        _download_and_extract(url["train"], "train2014.zip", path)
+        train_csv = _create_csv(train_data_dir)
+        train_csv.to_csv(train_csv_path, index=False)
+    else:
+        if not (len(glob(os.path.join(train_data_dir, '*.jpg'))) == NUM_IMG_FILES):
+            print("One or more images are missing")
+            _download_and_extract(url["train"], "train2014.zip", path)
+            train_csv = _create_csv(train_data_dir)
+            train_csv.to_csv(train_csv_path, index=False)
+        else:
+            if not (os.path.exists(train_csv_path)):
+                train_csv = _create_csv(train_data_dir)
+                train_csv.to_csv(train_csv_path, index=False)
+            else:
+                print("reusing existing dataset")
+    return train_csv_path, train_data_dir
