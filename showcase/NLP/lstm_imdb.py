623e7fac3f91f9601cf2b8a09ce6d015d39c1edb
Purujit Bahl <purujit.bahl@ge.com>
2019-08-15 13:08:04 -0700

    updated NLP example to fastestimator 2.0

--- showcase/NLP/lstm_imdb.py
+++ showcase/NLP/lstm_imdb.py
@@ -14,65 +14,66 @@
 # ==============================================================================
 import numpy as np
 import tensorflow as tf
-from fastestimator.pipeline.static.preprocess import Reshape
-from tensorflow.keras import layers
+import fastestimator as fe
 
-from fastestimator.estimator.estimator import Estimator
+from tensorflow.python.keras import layers
+from fastestimator.pipeline.processing import Reshape
+from fastestimator.network.model import ModelOp, build
+from fastestimator.network.loss import BinaryCrossentropy
 from fastestimator.estimator.trace import Accuracy
-from fastestimator.pipeline.pipeline import Pipeline
+
 
 MAX_WORDS = 1000
-MAX_LEN = 150
+MAX_LEN = 300
 
 
-class Network:
-    def __init__(self):
-        self.model = self.create_lstm()
-        self.optimizer = tf.optimizers.Adam()
-        self.loss = tf.losses.BinaryCrossentropy()
+def create_lstm():
+    model = tf.keras.Sequential()
+    model.add(layers.Embedding(MAX_WORDS, 100, input_length=MAX_LEN))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Conv1D(64, 5, activation='relu'))
+    model.add(layers.MaxPooling1D(pool_size=4))
+    model.add(layers.LSTM(100))
+    model.add(layers.Dense(1, activation="sigmoid"))
 
-    def train_op(self, batch):
-        with tf.GradientTape() as tape:
-            predictions = self.model(batch["x"])
-            loss = self.loss(batch["y"], predictions)
-        gradients = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
-        return predictions, loss
-
-    def eval_op(self, batch):
-        predictions = self.model(batch["x"], training=False)
-        loss = self.loss(batch["y"], predictions)
-        return predictions, loss
-
-    def create_lstm(self):
-        model = tf.keras.Sequential()
-        model.add(layers.Embedding(MAX_WORDS, 100, input_length=MAX_LEN))
-        model.add(layers.Dropout(0.2))
-        model.add(layers.Conv1D(64, 5, activation='relu'))
-        model.add(layers.MaxPooling1D(pool_size=4))
-        model.add(layers.LSTM(100))
-        model.add(layers.Dense(1, activation="sigmoid"))
-        return model
+    return model
 
 
-def pad(list, padding_size, padding_value):
-    return list + [padding_value] * abs((len(list) - padding_size))
+def pad(input_list, padding_size, padding_value):
+    return input_list + [padding_value] * abs((len(input_list) - padding_size))
 
 
 def get_estimator(epochs=10, batch_size=64, optimizer="adam"):
-    (x_train, y_train), (x_eval, y_eval) = tf.keras.datasets.imdb.load_data(maxlen=300, num_words=MAX_WORDS)
-    x_train = np.array([pad(x, 300, 0) for x in x_train])
-    x_eval = np.array([pad(x, 300, 0) for x in x_eval])
+    # step 1. prepare data
+    (x_train, y_train), (x_eval, y_eval) = tf.keras.datasets.imdb.load_data(maxlen=MAX_LEN, num_words=MAX_WORDS)
+    data = {
+        "train": {
+            "x": np.array([pad(x, MAX_LEN, 0) for x in x_train]),
+            "y": y_train
+        },
+        "eval": {
+            "x": np.array([pad(x, MAX_LEN, 0) for x in x_eval]),
+            "y": y_eval
+        }
+    }
 
-    pipeline = Pipeline(batch_size=batch_size, feature_name=["x", "y"], train_data={
-        "x": x_train,
-        "y": y_train
-    }, validation_data={
-        "x": x_eval,
-        "y": y_eval
-    }, transform_train=[[], [Reshape([1])]])
+    pipeline = fe.Pipeline(batch_size=batch_size, data=data, ops=Reshape([1], inputs="y", outputs="y"))
 
-    traces = [Accuracy(y_true_key="y")]
+    # step 2. prepare model
+    model = build(keras_model=create_lstm(),
+                  loss=BinaryCrossentropy(y_true="y", y_pred="y_pred"),
+                  optimizer=optimizer)
+    network = fe.Network(ops=ModelOp(inputs="x", model=model, outputs="y_pred"))
 
-    estimator = Estimator(network=Network(), pipeline=pipeline, epochs=epochs, traces=traces)
+    # step 3.prepare estimator
+    estimator = fe.Estimator(network=network,
+                             pipeline=pipeline,
+                             epochs=epochs,
+                             traces=Accuracy(true_key="y", pred_key="y_pred"))
+
     return estimator
+
+
+if __name__ == "__main__":
+    o = get_estimator()
+    o.fit()
