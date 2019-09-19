cd897d49e21031b724cf80741c5424bcb65fc7ef
Purujit Bahl <purujit.bahl@ge.com>
2019-09-19 11:27:44 -0700

    updated docstrings according to the comments

--- fastestimator/pipeline/pipeline.py
+++ fastestimator/pipeline/pipeline.py
@@ -33,11 +33,12 @@ class Pipeline:
     """
         Class representing the data pipeline required for fastestimator
         Args:
-            batch_size: Integer representing the batch size for training model
-            ops: List of fastestimator operations that needs to be applied on the data in the pipeline
+            data: The input for the pipeline. This can be either a dictionary, a tfrecord path or a RecordWriter.
+            batch_size: Integer representing the batch size per device for training the model.
+            ops: List of fastestimator operations that needs to be applied on the data in the pipeline.
             read_feature: List of features that should be used in training. If None all the features available are used.
-            padded_batch: Boolean representing if a batch should be padded or not
-            expand_dims: Boolean representing if a batch dimensions should be expanded or not
+            padded_batch: Boolean representing if a batch should be padded or not.
+            expand_dims: Boolean representing if a batch dimensions should be expanded or not.
             max_shuffle_buffer_mb: Maximum buffer size to shuffle data. This is used only if the number of examples are
                 more than that could fit in the buffer. Defaults to 3000.
     """
@@ -99,7 +100,7 @@ class Pipeline:
 
     def prepare(self, distribute_strategy=None):
         """
-        Transform the dataset by running all the ops specified for the pipeline.
+        Create the dataset used by the pipeline by running all the ops specified.
 
         Args:
             distribute_strategy: Type of strategy to be used for multi GPU setup.
@@ -344,7 +345,7 @@ class Pipeline:
 
     def get_global_batch_size(self, epoch):
         """
-        Gets the batch size for the current epoch. Batch size changes if there is a schedule which specifies a change
+        Gets the global batch size for the current epoch. Batch size changes if there is a schedule which specifies a change
         for the given epoch.
 
         Args:
@@ -378,8 +379,7 @@ class Pipeline:
 
     def benchmark(self, mode="train", num_steps=1000, log_interval=100, current_epoch=0):
         """
-        Runs benchmarks for the current epoch. Batch size changes if there is a schedule which specifies a change
-        for the currentepoch.
+        Runs benchmarks for the current epoch.
 
         Args:
             mode: can be either "train" or "eval".
