e94c5d402429a6d9a88aa8785e388233c086fc12
vivek <viveks2410@gmail.com>
2019-10-09 23:10:11 +0000

    updated test scripts

--- test/test_image_classification/test_lenet_mnist.sh
+++ test/test_image_classification/test_lenet_mnist.sh
@@ -1,42 +1,35 @@
 #!/bin/bash
 path_apphub='../apphub/'
-path_tabular=${path_apphub}'tabular/'
+path_image_classification=${path_apphub}'image_classification/'
 path_temp=$(dirname $(mktemp -u))
 
 #training parameters to test the models
 train_info='--epochs 2 --batch_size 2 --steps_per_epoch 10 --validation_steps 5 --model_dir None'
 nb_train_info='-p epochs 2 -p batch_size 2 -p steps_per_epoch 10 -p validation_steps 5' #notebook parameters
 
-cnt=0
-
-echo -en '\n'
-echo 'Tabular'
-echo 'DNN Housing'
-echo -en '\n'
-
-filepath=${path_tabular}'dnn_housing/'
-filename='dnn_housing.py'
+filepath=${path_image_classification}'lenet_mnist/'
+filename='lenet_mnist.py'
 
 if fastestimator train ${filepath}${filename} ${train_info}; then
     ((cnt=cnt+1))
 else
-    echo 'Testing failed on DNN Housing'
+    echo 'Testing failed on LeNet MNIST'
     exit 0
 fi
 
-nb_filename='dnn_housing.ipynb'
-nb_param_filename='/dnn_housing_param.ipynb'
+nb_filename='lenet_mnist.ipynb'
+nb_param_filename='/lenet_mnist_param.ipynb'
 #inject a parameter cell
 papermill --prepare-only ${filepath}${nb_filename}  ${path_temp}${nb_param_filename} ${nb_train_info}
-jupyter nbconvert --to script  ${path_temp}${nb_param_filename} --output 'dnn_housing_param'
+jupyter nbconvert --to script  ${path_temp}${nb_param_filename} --output 'lenet_mnist_param'
 
-if ipython  ${path_temp}'/dnn_housing_param.py'; then
+if ipython  ${path_temp}'/lenet_mnist_param.py'; then
     ((cnt=cnt+1))
 else
-    echo 'Testing failed on DNN Housing notebook'
+    echo 'Testing failed on LeNet MNIST notebook'
     exit 0
 fi
-rm -rf /tmp/tmp*
+rm -rf ${path_temp}/tmp*
 rm  ${path_temp}${nb_param_filename}
-rm  ${path_temp}'/dnn_housing_param.py'
+rm  ${path_temp}'/lenet_mnist_param.py'
 exit 1
---
+++ test/test_image_segmentation/test_unet_montgomery.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+path_apphub='../apphub/'
+path_image_segmentation=${path_apphub}'image_segmentation/'
+path_temp=$(dirname $(mktemp -u))
+
+#training parameters to test the models
+train_info='--epochs 2 --batch_size 2 --steps_per_epoch 10 --validation_steps 5 --model_dir None'
+nb_train_info='-p epochs 2 -p batch_size 2 -p steps_per_epoch 10 -p validation_steps 5' #notebook parameters
+
+filepath=${path_image_segmentation}'unet_montgomery/'
+filename='unet_montgomery.py'
+
+if fastestimator train ${filepath}${filename} ${train_info}; then
+    ((cnt=cnt+1))
+else
+    echo 'Testing failed on Unet mongomery'
+    exit 0
+fi
+
+nb_filename='unet_montgomery.ipynb'
+nb_param_filename='/unet_montgomery_param.ipynb'
+#inject a parameter cell
+papermill --prepare-only ${filepath}${nb_filename}  ${path_temp}${nb_param_filename} ${nb_train_info}
+jupyter nbconvert --to script  ${path_temp}${nb_param_filename} --output 'unet_montgomery_param'
+
+if ipython  ${path_temp}'/unet_montgomery_param.py'; then
+    ((cnt=cnt+1))
+else
+    echo 'Testing failed on UNET Montgomery notebook'
+    exit 0
+fi
+rm -rf ${path_temp}/tmp*
+rm  ${path_temp}${nb_param_filename}
+rm  ${path_temp}'/unet_montgomery_param.py'
+exit 1
