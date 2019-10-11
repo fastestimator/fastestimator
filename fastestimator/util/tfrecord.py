799d1d8854abb1125fa879293a4f7b998b17cc25
Hsi-Ming Chang <hsi-ming.chang@ge.com>
2019-10-11 10:50:38 -0700

    Clean up tfrecord.py

--- fastestimator/util/tfrecord.py
+++ fastestimator/util/tfrecord.py
@@ -12,22 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import tensorflow as tf
-import numpy as np
+"""Extract information from TFRecord."""
 import json
 import os
 
+import numpy as np
+import tensorflow as tf
+
 
 def get_number_of_examples(file_path, show_warning=True, compression=None):
-    """Returns number of examples in one TFRecord
+    """Return number of examples in one TFRecord.
 
     Args:
-        file_path: Path of TFRecord file
-        show_warning: Whether to display warning message
-        compression: None, 'GZIP' or 'ZLIB'
+        file_path (str): Path of TFRecord file.
+        show_warning (bool): Whether to display warning messages.
+        compression (str): TFRecord compression type: `None`, `'GZIP'`, or `'ZLIB'`.
 
     Returns:
-        Number of examples in the TFRecord
+        Number of examples in the TFRecord.
+
     """
     _, ext = os.path.splitext(file_path)
     assert "tfrecord" in ext, "please make sure data is in tfrecord format"
@@ -41,19 +44,20 @@ def get_number_of_examples(file_path, show_warning=True, compression=None):
 
 
 def get_features(file_path, compression=None):
-    """Returns the feature information in TFRecords
+    """Return the feature information in TFRecord.
 
     Args:
-        file_path: Path of TFRecord file
-        compression: None, 'GZIP' or 'ZLIB'
+        file_path (str): Path of TFRecord file
+        compression (str): TFRecord compression type: `None`, `'GZIP'`, or `'ZLIB'`.
 
     Returns:
-        Dictionary containing feature information of TFRecords
+        Dictionary containing feature information of TFRecords.
+
     """
-    def _get_dtype(ex, feature):
-        dtype = list(ex.features.feature[feature].DESCRIPTOR.fields_by_name.keys())
+    def _get_dtype(example, feature):
+        dtype = list(example.features.feature[feature].DESCRIPTOR.fields_by_name.keys())
         dtype = np.array(dtype)
-        feature_dtype = dtype[[ex.features.feature[feature].HasField(x) for x in dtype]]
+        feature_dtype = dtype[[example.features.feature[feature].HasField(x) for x in dtype]]
         feature_dtype = str(np.squeeze(feature_dtype))
         type_dict = {'bytes_list': tf.string, 'int64_list': tf.int64, 'float_list': tf.float32}
         tf_type = type_dict[feature_dtype]
@@ -62,7 +66,7 @@ def get_features(file_path, compression=None):
     _, ext = os.path.splitext(file_path)
     assert "tfrecord" in ext, "please make sure data is in tfrecord format"
     dataset = tf.data.TFRecordDataset(file_path, compression_type=compression)
-    example = tf.train.Example.FromString(next(iter(dataset)).numpy())
+    example = tf.train.Example.FromString(next(iter(dataset)).numpy())  # pylint: disable=no-member
     feature_list = list(example.features.feature.keys())
     tf_type_list = [tf.io.FixedLenFeature([], _get_dtype(example, f)) for f in feature_list]
     keys_to_features = dict(zip(feature_list, tf_type_list))
@@ -78,7 +82,7 @@ def add_summary(data_dir,
                 num_train_examples=None,
                 num_eval_examples=None,
                 compression=None):
-    """Adds summary.json file to existing path with tfrecords.
+    """Add summary.json file to existing directory that has TFRecord files.
 
     Args:
         data_dir (str): Folder path where tfrecords are stored.
@@ -92,27 +96,27 @@ def add_summary(data_dir,
         num_eval_examples (int, optional): The total number of validation examples, if None, it will calculate
             automatically. Defaults to None.
         compression (str, optional): None, 'GZIP' or 'ZLIB'. Defaults to None.
+
     """
     train_files = [f for f in os.listdir(data_dir) if f.startswith(train_prefix)]
-    assert len(train_files) > 0, "Couldn't find any training tfrecord files in %s" % data_dir
+    assert train_files, "Couldn't find any training tfrecord files in {}".format(data_dir)
     dataset = tf.data.TFRecordDataset(os.path.join(data_dir, train_files[0]), compression_type=compression)
-    example = tf.train.Example.FromString(next(iter(dataset)).numpy())
+    example = tf.train.Example.FromString(next(iter(dataset)).numpy())  # pylint: disable=no-member
     feature_list = list(example.features.feature.keys())
-    assert set(feature_list).issuperset(
-        set(feature_name)
-    ), "feature name should at least be subset of feature name in tfrecords, found %s , given %s." % (str(feature_list),
-                                                                                                      str(feature_name))
+    assert set(feature_list).issuperset(set(feature_name)), \
+        "feature name should at least be subset of feature name in tfrecords, found {}, given {}.".format( \
+        str(feature_list), str(feature_name))
     if not num_train_examples:
-        # num_train_examples = [get_number_of_examples(os.path.join(data_dir, f)) for f in train_files]
         num_trian_files = len(train_files)
         logging_interval = max(num_trian_files // 10, 1)
         num_train_examples = []
-        for i in range(num_trian_files):
-            if (i + 1) % logging_interval == 0:
-                print("FastEstimator: Calculating number of examples for train %d/%d" % (i + 1, num_trian_files))
+        for index in range(num_trian_files):
+            if (index + 1) % logging_interval == 0:
+                print("FastEstimator: Calculating number of examples for train {}/{}".format(
+                    index + 1, num_trian_files))
             num_train_examples.append(
-                get_number_of_examples(file_path=os.path.join(data_dir, train_files[i]),
-                                       show_warning=i == 0,
+                get_number_of_examples(file_path=os.path.join(data_dir, train_files[index]),
+                                       show_warning=index == 0,
                                        compression=compression))
     summary = {
         "feature_name": feature_name,
@@ -123,23 +127,23 @@ def add_summary(data_dir,
     }
     if eval_prefix:
         eval_files = [f for f in os.listdir(data_dir) if f.startswith(eval_prefix)]
-        assert len(eval_files) > 0, "Couldn't find any validation tfrecord files in %s" % data_dir
+        assert eval_files, "Couldn't find any validation tfrecord files in {}".format(data_dir)
         if not num_eval_examples:
-            # num_eval_examples = [get_number_of_examples(os.path.join(data_dir, f)) for f in eval_files]
             num_eval_files = len(eval_files)
             logging_interval = max(num_eval_files // 10, 1)
             num_eval_examples = []
-            for i in range(num_eval_files):
-                if (i + 1) % logging_interval == 0:
-                    print("FastEstimator: Calculating number of examples for eval %d/%d" % (i + 1, num_eval_files))
+            for index in range(num_eval_files):
+                if (index + 1) % logging_interval == 0:
+                    print("FastEstimator: Calculating number of examples for eval {}/{}".format(
+                        index + 1, num_eval_files))
                 num_eval_examples.append(
-                    get_number_of_examples(file_path=os.path.join(data_dir, eval_files[i]),
-                                           show_warning=i == 0,
+                    get_number_of_examples(file_path=os.path.join(data_dir, eval_files[index]),
+                                           show_warning=index == 0,
                                            compression=compression))
         summary["eval_files"] = eval_files
         summary["num_eval_examples"] = num_eval_examples
     if compression:
         summary["compression"] = compression
-    with open(os.path.join(data_dir, "summary0.json"), 'w') as fp:
-        json.dump(summary, fp, indent=4)
+    with open(os.path.join(data_dir, "summary0.json"), 'w') as output:
+        json.dump(summary, output, indent=4)
     print("FastEstimator: Writing " + str(os.path.join(data_dir, "summary0.json")))
