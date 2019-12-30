1a566bd1170f9398e308fa5e2c3396428acbb3e4
geez0219 <geez0219@gmail.com>
2019-12-30 18:47:41 +0000

    experiment

--- test/test_apphub_scripts/test_retinanet_coco.sh
+++ test/test_apphub_scripts/test_retinanet_coco.sh
@@ -15,19 +15,22 @@ mkdir $path_stderr_dir
 train_info='--epochs 2 --batch_size 2 --steps_per_epoch 10 --validation_steps 5'
 nb_train_info='-p BATCH_SIZE 10 -p EPOCHS 2 -p STEPS_PER_EPOCH 10 -p VALIDATION_STEPS 5' #notebook parameters
 
-if ! fastestimator train ${filepath}${fname}'.py' ${train_info} 2>> ${path_stderr_dir}'run_py.txt'; then
-    exit 1
-fi
+# if ! fastestimator train ${filepath}${fname}'.py' ${train_info} 2>> ${path_stderr_dir}'run_py.txt'; then
+#     exit 1
+# fi
 
 #inject a parameter cell
 papermill --prepare-only ${filepath}${fname}'.ipynb'  ${path_temp}${fname_para}'.ipynb' ${nb_train_info} 2>> ${path_stderr_dir}'run_papermill.txt'
 jupyter nbconvert --to script  ${path_temp}${fname_para}'.ipynb' --output ${fname_para} 2>> ${path_stderr_dir}'run_convert.txt'
 
-if ! ipython  ${path_temp}${fname_para}'.py' 2>> ${path_stderr_dir}'run_ipy.txt'; then
-    exit 1
-fi
-rm -rf ${path_temp}/tmp*
-rm  ${path_temp}${fname_para}'.ipynb'
-rm  ${path_temp}${fname_para}'.py'
+echo "generate the new ipynb at ${path_temp}${fname_para}.ipynb"
+
+# if ! ipython  ${path_temp}${fname_para}'.py' 2>> ${path_stderr_dir}'run_ipy.txt'; then
+#     exit 1
+# fi
+
+# rm -rf ${path_temp}/tmp*
+# rm  ${path_temp}${fname_para}'.ipynb'
+# rm  ${path_temp}${fname_para}'.py'
 
 exit 0
