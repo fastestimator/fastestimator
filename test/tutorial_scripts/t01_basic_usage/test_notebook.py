24703f37513e9990581ac659df1117c79e6b77e7
geez0219 <geez0219@gmail.com>
2020-01-08 18:19:54 -0800

    new tutorial test script

---
+++ test/tutorial_scripts/t01_basic_usage/test_notebook.py
@@ -0,0 +1,17 @@
+import os
+
+if __name__ == "__main__":
+    source_dir = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "tutorial"))
+    stderr_file = os.path.abspath(os.path.join(__file__, "..", "run_notebook.txt"))
+    if os.path.exists(stderr_file):
+        os.remove(stderr_file)
+
+    tutorial_name = "t01_basic_usage" # tutorial name without .ipynb extension
+    nb_in_file = os.path.join(source_dir, tutorial_name + ".ipynb")
+    nb_out_file = os.path.abspath(os.path.join(__file__, "..", tutorial_name + "_out.ipynb"))
+
+    run_info = "" # running parameter 
+    result = os.system("papermill {} {} {} 2>> {}".format(nb_in_file, nb_out_file, run_info, stderr_file))
+
+    if result:
+        raise ValueError("{} fail".format(nb_in_file))
