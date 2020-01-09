b5874a4277ab42f771ce23da2f04b9c98ab68b6c
geez0219 <geez0219@gmail.com>
2020-01-09 17:52:23 +0000

    clean the code

--- test/test_apphub_tutorial.py
+++ test/test_apphub_tutorial.py
@@ -6,30 +6,27 @@ import time
 if __name__ == "__main__":
     test_apphub_script_dir = os.path.abspath(os.path.join(__file__, "..", "apphub_scripts"))
     test_tutorial_script_dir = os.path.abspath(os.path.join(__file__, "..", "tutorial_scripts"))
-    print(test_apphub_script_dir)
-    print(test_tutorial_script_dir)
-
+    
     report={}
     fail_list=[]
-    # for dirpath, _, filenames in os.walk(test_apphub_script_dir):
-    #     if dirpath.split("/")[-1] == "template": 
-    #         continue
+    for dirpath, _, filenames in os.walk(test_apphub_script_dir):
+        if dirpath.split("/")[-1] == "template": 
+            continue
 
-    #     for f in filenames:
-    #         if f.split(".")[-1] != "py":
-    #             continue
+        for f in filenames:
+            if f.split(".")[-1] != "py":
+                continue
 
-    #         print(os.path.abspath(os.path.join(dirpath, f)))
-    #         test_name = os.path.join(dirpath.split("/")[-1], f)
-    #         start_time = time.time()
-    #         result = os.system("python3 " + os.path.abspath(os.path.join(dirpath, f)))
-    #         exec_time = (time.time() - start_time) / 60 
-    #         report[test_name] = {"fail": result, "time":exec_time}
-    #         if result:
-    #             fail_list.append(test_name)
+            print(os.path.abspath(os.path.join(dirpath, f)))
+            test_name = os.path.join(dirpath.split("/")[-1], f)
+            start_time = time.time()
+            result = os.system("python3 " + os.path.abspath(os.path.join(dirpath, f)))
+            exec_time = (time.time() - start_time) / 60 
+            report[test_name] = {"fail": result, "time":exec_time}
+            if result:
+                fail_list.append(test_name)
 
     for dirpath, _, filenames in os.walk(test_tutorial_script_dir):
-        print(dirpath)
         if dirpath.split("/")[-1] == "template": 
             continue
 
