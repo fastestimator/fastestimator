9c79d7b44e82dae71ca0d21003584ff9912ed9e9
geez0219 <geez0219@gmail.com>
2020-02-12 12:13:33 -0800

    complete template

--- test/tutorial_scripts/template/run_notebook.py
+++ test/tutorial_scripts/template/run_notebook.py
@@ -1,6 +1,8 @@
 import os
 
 if __name__ == "__main__":
+     """The template for running tutorial Jupyter notebook sample. Users only need to fill the sections with comments.
+    """
     source_dir = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "tutorial"))
     stderr_file = os.path.abspath(os.path.join(__file__, "..", "run_notebook.txt"))
     if os.path.exists(stderr_file):
@@ -10,7 +12,11 @@ if __name__ == "__main__":
     nb_in_file = os.path.join(source_dir, tutorial_name + ".ipynb")
     nb_out_file = os.path.abspath(os.path.join(__file__, "..", tutorial_name + "_out.ipynb"))
 
-    run_info = "" # running parameter 
+    run_info = "" # Running parameter
+    # Say if we want to set a=5 nad b=10, the expression for this is "-p a 5 -p b 10"
+    # The arguement will re-declare the variable right after the jupyter notebook cell with "parameters" tag (there must
+    # be one and only cell with "parameters" tag)
+
     result = os.system("papermill {} {} {} -k nightly-build 2>> {}".format(nb_in_file, nb_out_file, run_info, stderr_file))
 
     if result:
