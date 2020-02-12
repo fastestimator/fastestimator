028f465cdea273a46dc4b817d4b110c8070a60f7
geez0219 <geez0219@gmail.com>
2020-02-12 12:07:07 -0800

    complete the template

--- test/apphub_scripts/template/run_python.py
+++ test/apphub_scripts/template/run_python.py
@@ -1,17 +1,23 @@
 import os
 
 if __name__ == "__main__":
+    """The template for running apphub Jupyter notebook sample. Users only need to fill the sections with comments.
+    """
     apphub_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "apphub"))
+    example_name = ""  # The name of the running example. It should match the python file name.
 
-    example_name = ""
-    source_dir = os.path.join(apphub_path, "", "")
+    source_dir = os.path.join(apphub_path, "", "")  # The path to that example directory
     stderr_file = os.path.abspath(os.path.join(__file__, "..", "run_python.txt"))
     if os.path.exists(stderr_file):
         os.remove(stderr_file)
-        
+
     py_file = os.path.join(source_dir, example_name + ".py")
 
-    train_info = "--epochs 2 --batch_size 2"
+    train_info = ""  # The training arguments
+    # Usually we set the epochs:2, batch_size:2, steps_per_epoch:10, validation_steps:5
+    # The expression for the following setup is "--epochs 2 --batch_size 2 --steps_per_epoch 10 --validation_steps 5"
+    # The syntax of this expression is different from run_notebook.py
+
     result = os.system("fastestimator train {} {} 2>> {}".format(py_file, train_info, stderr_file))
 
     if result:
