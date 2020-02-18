d43c261cda7952f250597d4875254816547c5876
geez0219 <geez0219@gmail.com>
2020-02-18 22:57:58 +0000

    added types

--- fastestimator/trace/metric/recall.py
+++ fastestimator/trace/metric/recall.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 from sklearn.metrics import recall_score
@@ -26,28 +26,28 @@ class Recall(Trace):
     """Compute recall for classification task and report it back to logger.
 
     Args:
-        true_key (str): Name of the keys in the ground truth label in data pipeline.
+        true_key: Name of the keys in the ground truth label in data pipeline.
         pred_key (str, optional): Name of the keys in predicted label. Defaults to None.
         labels (list, optional): The set of labels to include. For more details, please refer to
-            sklearn.netrics.recall_score. Defaults to None.
+            sklearn.metrics.recall_score. Defaults to None.
         pos_label (str or int, optional): The class to report. For more details, please refer to
-            sklearn.netrics.recall_score. Defaults to 1.
+            sklearn.metrics.recall_score. Defaults to 1.
         average (str, optional): It should be one of {"auto", "binary", "micro", "macro", "weighted", "samples", None}.
             If "auto", the trace will detect the input data type and choose the right average for you. Otherwise, it
             will pass its to sklearn.metrics.recall_score. Defaults to "auto".
         sample_weight (array-like of shape, optional): Sample weights, For more details, please refer to
-            sklearn.netrics.recall_score. Defaults to None.
+            sklearn.metrics.recall_score. Defaults to None.
         mode (str, optional): Restrict the trace to run only on given modes {'train', 'eval', 'test'}. None will always
                     execute. Defaults to 'eval'.
         output_name (str, optional): Name of the key to store to the state. Defaults to "recall".
     """
     def __init__(self,
                  true_key: str,
-                 pred_key: str = None,
-                 labels: str = None,
-                 pos_label: int = 1,
-                 average: str = 'auto',
-                 sample_weight: str = None,
+                 pred_key: Optional[Union[None, str]] = None,
+                 labels: Optional[Union[None, str]] = None,
+                 pos_label: Optional[Union[int, str]] = 1,
+                 average: Optional[Union[None, str]] = 'auto',
+                 sample_weight: Optional[Union[None, np.ndarray]] = None,
                  mode: Optional[str] ="eval",
                  output_name: Optional[str] = "recall"):
 
