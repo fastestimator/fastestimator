006555f2a0ccd228becd6d0502f052368699dc4c
geez0219 <geez0219@gmail.com>
2020-02-18 23:58:27 +0000

    add type hinting

--- fastestimator/trace/metric/precision.py
+++ fastestimator/trace/metric/precision.py
@@ -14,7 +14,7 @@
 # ==============================================================================
 import numpy as np
 from sklearn.metrics import precision_score
-from typing import Optional
+from typing import Optional, Union, List
 
 from fastestimator.backend.to_number import to_number
 from fastestimator.trace.trace import Trace
@@ -25,30 +25,34 @@ class Precision(Trace):
     """Computes precision for classification task and report it back to logger.
 
     Args:
-        true_key (str): Name of the keys in the ground truth label in data pipeline.
-        pred_key (str, optional): Name of the keys in predicted label. Defaults to None.
-        labels (list, optional): The set of labels to include. For more details please refer to
+        true_key: Name of the keys in the ground truth label in data pipeline.
+        pred_key: Name of the keys in predicted label. Defaults to None.
+        labels: The set of labels to include. For more details please refer tosklearn.metrics.precision_score. Defaults
+            to None.
+        pos_label: The class to report. For more details please refer to sklearn.metrics.precision_score. Defaults to 1.
+        average: It should be one of {"auto", "binary", "micro", "macro", "weighted", "samples", None}. If "auto", the
+            trace will detect the input data type and choose the right average for you. Otherwise, it will pass its to
+            sklearn.metrics.precision_score. Defaults to "auto".
+        sample_weight: Sample weight. If None, it will not apply sample weight. For more details please refer to
             sklearn.metrics.precision_score. Defaults to None.
-        pos_label (str or int, optional): The class to report. For more details please refer to
-            sklearn.metrics.precision_score. Defaults to 1.
-        average (str, optional): It should be one of {"auto", "binary", "micro", "macro", "weighted", "samples", None}.
-            If "auto", the trace will detect the input data type and choose the right average for you. Otherwise, it
-            will pass its to sklearn.metrics.precision_score. Defaults to "auto".
-        sample_weight (1d array-like, optional): Sample weight. If None, it will not apply sample weight. For more
-            details please refer to sklearn.metrics.precision_score. Defaults to None.
-        mode (str, optional): Restrict the trace to run only on given modes {'train', 'eval', 'test'}. None will always
-                    execute. Defaults to 'eval'.
-        output_name (str, optional): Name of the key to store to the state. Defaults to "precision".
+        mode: Restrict the trace to run only on given modes {'train', 'eval', 'test'}. None will always execute.
+            Defaults to 'eval'.
+        output_name: Name of the key to store to the state. Defaults to "precision".
     """
+
+    y_true: List[int]
+    y_pred: List[int]
+
     def __init__(self,
                  true_key: str,
                  pred_key: str,
-                 labels=None,
-                 pos_label=1,
-                 average='auto',
-                 sample_weight=None,
-                 mode: Optional[str] = "eval",
-                 output_name: str = "precision"):
+                 labels: Optional[list] = None,
+                 pos_label: Union[int, str] = 1,
+                 average: str = 'auto',
+                 sample_weight: Optional[np.ndarray] = None,
+                 mode: str = "eval",
+                 output_name: Optional[str] = "precision"):
+                 
         super().__init__(inputs=(true_key, pred_key), outputs=output_name, mode=mode)
         self.labels = labels
         self.pos_label = pos_label
