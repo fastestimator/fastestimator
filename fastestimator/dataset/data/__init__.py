6a309551fcf5cd22485a16979bd8b3ef6b6f6089
Potter, Michael (GE Digital) <michael.potter@ge.com>
2020-02-19 09:47:45 -0800

    dataset as function

--- fastestimator/dataset/data/__init__.py
+++ fastestimator/dataset/data/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from fastestimator.dataset.data.usps_dataset import USPSDataset
+from fastestimator.dataset.data import usps
