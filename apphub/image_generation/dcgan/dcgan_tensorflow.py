7ddd01158b803baf17b76910a0e6df02065b4c15
vbvg2008 <shawnmengdong@gmail.com>
2020-02-27 12:00:48 -0700

    dcgan p1

---
+++ apphub/image_generation/dcgan/dcgan_tensorflow.py
@@ -0,0 +1,100 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""DCGAN example using MNIST data set."""
+
+import pdb
+
+import tensorflow as tf
+from tensorflow.python.keras import layers
+
+import fastestimator as fe
+from fastestimator.backend import cross_entropy
+from fastestimator.dataset import mnist
+from fastestimator.op import TensorOp
+from fastestimator.op.numpyop import ExpandDims, Normalize
+from fastestimator.op.tensorop.loss import CrossEntropy
+from fastestimator.op.tensorop.model import ModelOp, UpdateOp
+
+
+class GLoss(TensorOp):
+    def forward(self, data, state):
+        return cross_entropy(y_pred=data, y_true=tf.ones_like(data), from_logits=True)
+
+
+class DLoss(TensorOp):
+    def forward(self, data, state):
+        true_score, fake_score = data
+        real_loss = cross_entropy(y_pred=true_score, y_true=tf.ones_like(true_score), from_logits=True)
+        fake_loss = cross_entropy(y_pred=fake_score, y_true=tf.zeros_like(fake_score), from_logits=True)
+        total_loss = real_loss + fake_loss
+        return total_loss
+
+
+def generator():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100, )))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Reshape((7, 7, 256)))
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    return model
+
+
+def discriminator():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+    return model
+
+
+def get_estimator(batch_size=256):
+    train_data, _ = mnist.load_data()
+    pipeline = fe.Pipeline(
+        train_data=train_data,
+        batch_size=batch_size,
+        ops=[
+            ExpandDims(inputs="x", outputs="x"),
+            Normalize(inputs="x", outputs="x", mean=1.0, std=1.0, max_pixel_value=127.5)
+        ])
+    gen_model = fe.build(model=generator(), optimizer=tf.optimizers.Adam(1e-4))
+    disc_model = fe.build(model=discriminator(), optimizer=tf.optimizers.Adam(1e-4))
+    network = fe.Network(ops=[
+        ModelOp(model=gen_model, inputs=lambda: tf.random.normal([batch_size, 100]), outputs="x_fake"),
+        ModelOp(model=disc_model, inputs="x_fake", outputs="fake_score"),
+        ModelOp(inputs="x", model=disc_model, outputs="true_score"),
+        GLoss(inputs="fake_score", outputs="gloss"),
+        UpdateOp(model=gen_model, loss_name="gloss"),
+        DLoss(inputs=("true_score", "fake_score"), outputs="dloss"),
+        UpdateOp(model=disc_model, loss_name="dloss")
+    ])
+    estimator = fe.Estimator(pipeline=pipeline, network=network, epochs=2)
+    return estimator
+
+
+if __name__ == "__main__":
+    est = get_estimator()
+    est.fit()
--- fastestimator/op/tensorop/loss/cross_entropy.py
+++ fastestimator/op/tensorop/loss/cross_entropy.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from typing import Union, Iterable, Callable, List, TypeVar, Dict, Any
+from typing import Any, Callable, Dict, Iterable, List, TypeVar, Union
 
 import tensorflow as tf
 import torch
@@ -31,22 +31,22 @@ class CrossEntropy(TensorOp):
         inputs: A tuple or list like: [<y_pred>, <y_true>]
         outputs: key to store the computed loss value
         mode: 'train', 'eval' or None
-        apply_softmax: whether to apply softmax to y_pred. Defaults to False.
+        from_logits: whether y_pred is logits (without softmax). Defaults to False.
         average_loss: whether to average the element-wise loss after the Loss Op
     """
     def __init__(self,
                  inputs: Union[None, str, Iterable[str], Callable] = None,
                  outputs: Union[None, str, Iterable[str]] = None,
                  mode: Union[None, str, Iterable[str]] = None,
-                 apply_softmax: bool = False,
+                 from_logits: bool = False,
                  average_loss: bool = True):
-        self.apply_softmax = apply_softmax
+        self.from_logits = from_logits
         self.average_loss = average_loss
         super().__init__(inputs=inputs, outputs=outputs, mode=mode)
 
     def forward(self, data: List[Tensor], state: Dict[str, Any]) -> Tensor:
         y_pred, y_true = data
-        loss = cross_entropy(y_pred, y_true, apply_softmax=self.apply_softmax)
+        loss = cross_entropy(y_pred, y_true, from_logits=self.from_logits)
         if self.average_loss:
             loss = reduce_loss(loss)
         return loss
