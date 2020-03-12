f1fb4940610583c2aeb751bb6198fbd2f88f792a
Vivek <viveks2410@gmail.com>
2020-03-12 22:20:00 +0000

    fixed input datatype in reshape op

--- fastestimator/op/numpyop/univariate/reshape.py
+++ fastestimator/op/numpyop/univariate/reshape.py
@@ -1,3 +1,4 @@
+import pdb
 from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
 
 import numpy as np
@@ -18,7 +19,11 @@ class Reshape(NumpyOp):
                  mode: Union[None, str, Iterable[str]] = None):
         super().__init__(inputs=inputs, outputs=outputs, mode=mode)
         self.shape = shape
+        self.in_list, self.out_list = True, True
 
     def forward(self, data: List[np.ndarray], state: Dict[str, Any]) -> List[np.ndarray]:
+        return [self._apply_reshape(elem) for elem in data]
+
+    def _apply_reshape(self, data):
         data = np.reshape(data, self.shape)
         return data
