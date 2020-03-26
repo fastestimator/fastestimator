62709c5b6f3e6e329711783224c5b8ed7a617d5b
geez0219 <geez0219@gmail.com>
2020-03-26 01:04:36 +0000

    new night-test build

---
+++ test/apphub_scripts/mnist/run_tf.py
@@ -0,0 +1,31 @@
+import os
+
+if __name__ == "__main__":
+    """The template for running apphub Jupyter notebook sample.
+    Users only need to fill the SELF-FILLED SECTION.
+    """
+    apphub_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "apphub"))
+
+    # =================================  SELF-FILLED SECTION  =====================================
+    # The name of the running example file. (without training "_tf.py" )
+    example_name = "mnist"
+
+    # The path to that example directory
+    source_dir = os.path.join(apphub_path, "image_classification", "mnist")
+
+    # The training arguments
+    # 1. Usually we set the epochs:2, batch_size:2, max_steps_per_epoch:10
+    # 2. The expression for the following setup is "--epochs 2 --batch_size 2 --max_steps_per_epoch 10"
+    # 3. The syntax of this expression is different from run_notebook.py
+    train_info = "--epochs 2 --batch_size 2 --max_steps_per_epoch 10"
+    # ==============================================================================================
+
+    stderr_file = os.path.abspath(os.path.join(__file__, "..", "run_tf_stderr.txt"))
+    if os.path.exists(stderr_file):
+        os.remove(stderr_file)
+
+    py_file = os.path.join(source_dir, example_name + "_tf.py")
+    result = os.system("fastestimator train {} {} 2>> {}".format(py_file, train_info, stderr_file))
+
+    if result:
+        raise ValueError("{} fail".format(py_file))
