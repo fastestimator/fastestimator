765481fe80ad1829d23de04c943d120928050f65
geez0219 <geez0219@gmail.com>
2020-03-27 21:32:27 +0000

    new testing scripts

--- test/apphub_scripts/image_classification/cifar10_fast/run_tf.py
+++ test/apphub_scripts/image_classification/cifar10_fast/run_tf.py
@@ -1,17 +1,45 @@
 import os
 
+
+def get_apphub_path():
+    apphub_path = None
+    current_dir = os.path.abspath(os.path.join(__file__, ".."))
+    while current_dir != "/":
+        current_dir = os.path.abspath(os.path.join(current_dir, ".."))
+        if "apphub" in os.listdir(current_dir):
+            apphub_path = os.path.abspath(os.path.join(current_dir, "apphub"))
+            break
+
+    if apphub_path is None:
+        raise OSError("Could not find the apphub directory")
+
+    return apphub_path
+
+
+def get_relative_path():
+    current_dir = os.path.abspath(os.path.join(__file__, ".."))
+    split = current_dir.split("apphub_scripts/")
+    if len(split) == 1:
+        raise OSError("This file need to be put inside apphub_scripts directory")
+
+    return split[-1]
+
+
+def get_source_dir_path():
+    apphub_path = get_apphub_path()
+    relative_dir_path = get_relative_path()
+    source_dir_path = os.path.join(apphub_path, relative_dir_path)
+
+    return source_dir_path
+
+
 if __name__ == "__main__":
     """The template for running apphub Jupyter notebook sample.
     Users only need to fill the SELF-FILLED SECTION.
     """
-    apphub_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "apphub"))
-
     # =================================  SELF-FILLED SECTION  =====================================
     # The name of the running example file. (without training "_tf.py" )
-    example_name = "cvae"
-
-    # The path to that example directory
-    source_dir = os.path.join(apphub_path, "image_generation", "cvae")
+    example_name = "cifar10_fast"
 
     # The training arguments
     # 1. Usually we set the epochs:2, batch_size:2, max_steps_per_epoch:10
@@ -24,6 +52,7 @@ if __name__ == "__main__":
     if os.path.exists(stderr_file):
         os.remove(stderr_file)
 
+    source_dir = get_source_dir_path()
     py_file = os.path.join(source_dir, example_name + "_tf.py")
     result = os.system("fastestimator train {} {} 2>> {}".format(py_file, train_info, stderr_file))
 
