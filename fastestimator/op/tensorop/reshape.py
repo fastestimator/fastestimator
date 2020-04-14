137f64133cf38cb44586ea67c7bf3d286525de32
Vivek <viveks2410@gmail.com>
2020-04-14 22:52:07 +0000

    updated docstring in reshape op

--- fastestimator/op/tensorop/reshape.py
+++ fastestimator/op/tensorop/reshape.py
@@ -23,16 +23,15 @@ Tensor = TypeVar('Tensor', tf.Tensor, torch.Tensor)
 
 
 class Reshape(TensorOp):
-    """ReshapeOp defines the reshape operation that is performed on prediction and ground truth before passing them to
-    loss calculation.
+    """Reshape a input tensor to conform to a given shape.
 
     Args:
         inputs: Key of input tensor that is to be reshaped.
         outputs: Key of output tensor that are reshaped.
         shape: Target shape
         mode: What mode(s) to execute this Op in. For example, "train", "eval", "test", or "infer". To execute
-        regardless of mode, pass None. To execute in all modes except for a particular one, you can pass an argument
-        like "!infer" or "!train".
+            regardless of mode, pass None. To execute in all modes except for a particular one, you can pass an argument
+            like "!infer" or "!train".
     """
     def __init__(self,
                  inputs: Union[str, List[str]],
