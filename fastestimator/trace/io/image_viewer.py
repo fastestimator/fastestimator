10bd20800777c9f9d7f428e86e6ac9ef1cb6208f
Michael Potter <Michael.Potter@GE.com>
2020-04-24 13:27:45 -0700

    XAI

--- fastestimator/trace/io/image_viewer.py
+++ fastestimator/trace/io/image_viewer.py
@@ -24,21 +24,33 @@ from fastestimator.xai.util import XaiData, show_image
 class ImageViewer(Trace):
     """A trace that interrupts your training in order to display images on the screen.
 
+    This class is useful primarily for Jupyter Notebook, or for debugging purposes.
+
     Args:
-        inputs: Key(s) of images to be saved.
+        inputs: Key(s) of images to be displayed.
         mode: What mode(s) to execute this Trace in. For example, "train", "eval", "test", or "infer". To execute
             regardless of mode, pass None. To execute in all modes except for a particular one, you can pass an argument
             like "!infer" or "!train".
+        width: The width in inches of the figure.
+        height: The height in inches of the figure.
     """
-    def __init__(self, inputs: Union[str, Sequence[str]], mode: Union[str, Set[str]] = ("eval", "test")) -> None:
+    def __init__(self,
+                 inputs: Union[str, Sequence[str]],
+                 mode: Union[str, Set[str]] = ("eval", "test"),
+                 width: int = 12,
+                 height: int = 6) -> None:
         super().__init__(inputs=inputs, mode=mode)
+        plt.rcParams['figure.figsize'] = [width, height]
 
     def on_epoch_end(self, data: Data) -> None:
         for key in self.inputs:
             if key in data:
                 imgs = data[key]
                 if isinstance(imgs, XaiData):
-                    imgs.paint_figure()
+                    fig = imgs.paint_numpy(dpi=96)
+                    plt.imshow(fig[0])
+                    plt.axis('off')
+                    plt.tight_layout()
                     plt.show()
                 else:
                     for idx, img in enumerate(imgs):
