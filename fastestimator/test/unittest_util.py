fd92bbc12996023d8e9a50efea8d9bfe75163fb1
geez0219 <geez0219@gmail.com>
2020-06-01 15:22:02 -0700

    add new unit_test integration test

--- fastestimator/test/unittest_util.py
+++ fastestimator/test/unittest_util.py
@@ -4,7 +4,6 @@ import tensorflow as tf
 import torch
 from typing import Any
 
-
 def is_equal(obj1: Any, obj2: Any) -> bool:
     """Check whether input objects are equal. The object type can be nested iterable (list, tuple, set, dict) and
     with elements such as int, float, np.ndarray, tf.Tensor, tf.Varaible, torch.Tensor
@@ -55,3 +54,54 @@ def is_equal(obj1: Any, obj2: Any) -> bool:
 
     else:
         return obj1 == obj2
+
+
+def one_layer_tf_model() -> tf.keras.Model:
+    """Tensorflow Model with one dense layer without activation function.
+    * Model input shape: (3,)
+    * Model output: (1,)
+    * dense layer weight: [1.0, 2.0, 3.0]
+
+    How to feed_forward this model
+    ```python
+    model = one_layer_tf_model()
+    x = tf.constant([[1.0, 1.0, 1.0], [1.0, -1.0, -0.5]])
+    b = fe.backend.feed_forward(model, x) # [[6.0], [-2.5]]
+    ```
+
+    Returns:
+        tf.keras.Model: The model
+    """
+    input = tf.keras.layers.Input([3])
+    x = tf.keras.layers.Dense(units=1, use_bias=False)(input)
+    model = tf.keras.models.Model(inputs=input, outputs=x)
+    model.layers[1].set_weights([np.array([[1.0], [2.0], [3.0]])])
+    return model
+
+
+class OneLayerTorchModel(torch.nn.Module):
+    """Torch Model with one dense layer without activation function.
+    * Model input shape: (3,)
+    * Model output: (1,)
+    * dense layer weight: [1.0, 2.0, 3.0]
+
+    How to feed_forward this model
+    ```python
+    model = OneLayerTorchModel()
+    x = torch.tensor([[1.0, 1.0, 1.0], [1.0, -1.0, -0.5]])
+    b = fe.backend.feed_forward(model, x) # [[6.0], [-2.5]]
+    ```
+
+    Args:
+        torch ([type]): The model
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.fc1 = torch.nn.Linear(3, 1, bias=False)
+        self.fc1.weight.data = torch.tensor([[1, 2, 3]], dtype=torch.float32)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.fc1(x)
+        return x
+
+
