e25f6f606cff0a575bc2d9985350e12121313510
geez0219 <geez0219@gmail.com>
2020-06-03 21:50:22 -0700

    update test

--- test/PR_test/unit_test/util/test_util.py
+++ test/PR_test/unit_test/util/test_util.py
@@ -7,93 +7,92 @@ import unittest
 import time
 from unittest.mock import patch
 from io import StringIO
-
+import pdb
+from fastestimator.test.unittest_util import is_equal
 class TestParseStringToPython(unittest.TestCase):
-    def test_parse_string_to_python_number(self):
-        obj = 5
-        x = fe.util.parse_string_to_python(str(obj))
-        self.assertTrue(x == obj)
+    def test_parse_string_to_python_positive_int(self):
+        x = fe.util.parse_string_to_python("5")
+        self.assertEqual(x, 5)
 
-        obj = -1.5
-        x = fe.util.parse_string_to_python(str(obj))
-        self.assertTrue(x == obj)
+    def test_parse_string_to_python_negative_float(self):
+        x = fe.util.parse_string_to_python("-1.5")
+        self.assertEqual(x, -1.5)
 
     def test_parse_string_to_python_list(self):
-        obj = [5, 4, 0.3]
-        x = fe.util.parse_string_to_python(str(obj))
-        self.assertTrue(x == obj)
+        x = fe.util.parse_string_to_python("[5, 4, 0.3]")
+        self.assertTrue(x, [5, 4, 0.3])
 
-        obj = [5, [1,2,3]]
-        x = fe.util.parse_string_to_python(str(obj))
-        self.assertTrue(x == obj)
+    def test_parse_string_to_python_list_over_mixed_entries(self):
+        x = fe.util.parse_string_to_python("[5, [1,2,3]]")
+        self.assertTrue(x, [5, [1,2,3]])
 
     def test_parse_string_to_python_dict(self):
-        obj = {'a': 5, 'b': 7}
-        x = fe.util.parse_string_to_python(str(obj))
-        self.assertTrue(x == obj)
+        x = fe.util.parse_string_to_python("{'a': 5, 'b': 7}")
+        self.assertEqual(x, {'a': 5, 'b': 7})
 
-        obj = {'a':5, 'b':[1,2,3]}
-        x = fe.util.parse_string_to_python(str(obj))
-        self.assertTrue(x == obj)
+    def test_parse_string_to_python_dict_over_mixed_entries(self):
+        x = fe.util.parse_string_to_python("{'a':5, 'b':[1,2,3]}")
+        self.assertEqual(x, {'a': 5, 'b':[1,2,3]})
 
 class TestToList(unittest.TestCase):
     def test_to_list_input_none(self):
         x = fe.util.to_list(None)
-        self.assertTrue(x == [])
+        self.assertEqual(x, [])
 
-    def test_to_list_input_non_iterable(self):
-        x = fe.util.to_list(0)
-        self.assertTrue(x == [0])
+    def test_to_list_input_float(self):
+        x = fe.util.to_list(0.5)
+        self.assertEqual(x, [0.5])
 
+    def test_to_list_input_string(self):
         x = fe.util.to_list("a")
-        self.assertTrue(x == ["a"])
+        self.assertEqual(x, ["a"])
 
     def test_to_list_input_list(self):
         x = fe.util.to_list([None, "2", (3, 4), {5, 6}, {"7":7, "8":8}, [9]])
-        self.assertTrue(x == [None, "2", (3, 4), {5, 6}, {"7":7, "8":8}, [9]])
+        self.assertEqual(x, [None, "2", (3, 4), {5, 6}, {"7":7, "8":8}, [9]])
 
     def test_to_list_input_tuple(self):
         x = fe.util.to_list((None, "2", (3, 4), {5, 6}, {"7": 7, "8": 8}, [9]))
-        self.assertTrue(x == [None, "2", (3, 4), {5, 6}, {"7": 7, "8": 8}, [9]])
+        self.assertEqual(x, [None, "2", (3, 4), {5, 6}, {"7": 7, "8": 8}, [9]])
 
     def test_to_list_input_set(self):
         x = fe.util.to_list({3,4})
-        self.assertTrue(x == [3,4])
+        self.assertEqual(x, [3,4])
 
     def test_to_list_input_dict(self):
         x = fe.util.to_list({"ans": 1})
-        self.assertTrue(x == [{"ans": 1}])
+        self.assertEqual(x, [{"ans": 1}])
 
 class TestToSet(unittest.TestCase):
     def test_to_set_input_none(self):
         x = fe.util.to_set(None)
-        self.assertTrue(x == set())
+        self.assertEqual(x, set())
 
     def test_to_set_input_non_iterable(self):
         x = fe.util.to_set(0)
-        self.assertTrue(x == {0})
+        self.assertEqual(x, {0})
 
         x = fe.util.to_set("a")
-        self.assertTrue(x == {"a"})
+        self.assertEqual(x, {"a"})
 
     def test_to_set_input_list(self):
         x = fe.util.to_set([None, "2", (3, 4)])
-        self.assertTrue(x == {None, "2", (3, 4)})
+        self.assertEqual(x, {None, "2", (3, 4)})
 
     def test_to_set_input_tuple(self):
         x = fe.util.to_set((None, "2", (3, 4)))
-        self.assertTrue(x == {None, "2", (3, 4)})
+        self.assertEqual(x, {None, "2", (3, 4)})
 
     def test_to_set_input_set(self):
         x = fe.util.to_set({3,4})
-        self.assertTrue(x == {3,4})
+        self.assertEqual(x, {3,4})
 
 class TestNonContext(unittest.TestCase):
     def test_non_context_syntax_work(self):
         a = 5
         with fe.util.NonContext():
             a = a + 37
-            self.assertTrue(a==42)
+            self.assertEqual(a, 42)
 
 class TestSuppressor(unittest.TestCase):
     def test_suppressor(self):
@@ -102,10 +101,6 @@ class TestSuppressor(unittest.TestCase):
                 print("hello world")
             log = fake_stdout.getvalue()
             self.assertEqual(log, '')
-
-            print("hello world")
-            log = fake_stdout.getvalue()
-            self.assertEqual(log, "hello world\n")
 
 class TestTimer(unittest.TestCase):
     def test_timer_as_context_manager(self):
@@ -153,6 +148,14 @@ class TestPrettifyMetricName(unittest.TestCase):
     def test_prettify_metric_name(self):
         x = fe.util.prettify_metric_name("myUgly_loss")
         self.assertEqual(x, "My Ugly Loss")
+
+    def test_prettify_metric_name_all_capital(self):
+        x = fe.util.prettify_metric_name("ABCD")
+        self.assertEqual(x, "Abcd")
+
+    def test_prettify_metric_name_two_space(self):
+        x = fe.util.prettify_metric_name("hello  world")
+        self.assertEqual(x, "Hello World")
 
 class TestStripSuffix(unittest.TestCase):
     def test_strip_suffix_match(self):
@@ -230,10 +233,32 @@ class TestGetShape(unittest.TestCase):
         self.assertEqual(x, [None])
 
 
-    ```python
-    x = fe.util.get_shape(np.ones((12,22,11)))  # [12, 22, 11]
-    x = fe.util.get_shape([np.ones((12,22,11)), np.ones((18, 5))])  # [None]
-    x = fe.util.get_shape([np.ones((12,22,11)), np.ones((18, 5, 4))])  # [None, None, None, None]
-    x = fe.util.get_shape([np.ones((12,22,11)), np.ones((12, 22, 4))])  # [None, 12, 22, None]
-    x = fe.util.get_shape({"a": np.ones((12,22,11))})  # []
-    ```
+class TestPadBatch(unittest.TestCase):
+    def test_pad_batch_pad_one_entry(self):
+        data = [{"x": np.ones((2, 2)), "y": 8}, {"x": np.ones((3, 1)), "y": 4}]
+        fe.util.pad_batch(data, pad_value=0)
+        obj = [{"x": np.array([[1.,1.], [1., 1.], [0.,0.]]), "y": 8},
+                {"x": np.array([[1.,0.], [1., 0.], [1.,0.]]), "y": 4}]
+
+        self.assertTrue(is_equal(data, obj))
+
+    def test_pad_batch_pad_all_entry(self):
+        data = [{"x": np.ones((3, 1)), "y": np.ones((1, 1))}, {"x": np.ones((2, 2)), "y": np.ones((1, 3))}]
+        fe.util.pad_batch(data, pad_value=0)
+        obj = [{"x": np.array([[1.,0.], [1., 0.], [1.,0.]]), "y": np.array([[1., 0., 0.]])},
+               {"x": np.array([[1.,1.], [1., 1.], [0.,0.]]), "y": np.array([[1., 1., 1.]])}]
+
+        self.assertTrue(is_equal(data, obj))
+
+    def test_pad_batch_different_different_key_assertion(self):
+        data = [{"x1": np.ones((2, 2)), "y": 8}, {"x": np.ones((3, 1)), "y": 4}]
+        with self.assertRaises(AssertionError):
+            fe.util.pad_batch(data, pad_value=0)
+
+    def test_pad_batch_different_rank_mismatch_assertion(self):
+        data = [{"x1": np.ones((2, 2, 2)), "y": 8}, {"x": np.ones((3, 1)), "y": 4}]
+        with self.assertRaises(AssertionError):
+            fe.util.pad_batch(data, pad_value=0)
+
+class TestPadData(unittest.TestCase):
+    
