f717fba1dd88a8706f5064ad7a3ebe07ffe0b3f0
vbvg2008 <shawnmengdong@gmail.com>
2020-06-05 13:29:08 -0700

    added docstring

--- fastestimator/op/op.py
+++ fastestimator/op/op.py
@@ -108,6 +108,16 @@ def write_outputs_by_op(op: Op, store: MutableMapping[str, Any], outputs: Any) -
 
 @traceable()
 class LambdaOp(Op):
+    """An Operator that performs any specified function as forward function.
+
+    Args:
+        fn: The function to be executed.
+        inputs: Key(s) from which to retrieve data from the data dictionary.
+        outputs: Key(s) under which to write the outputs of this Op back to the data dictionary.
+        mode: What mode(s) to execute this Op in. For example, "train", "eval", "test", or "infer". To execute
+            regardless of mode, pass None. To execute in all modes except for a particular one, you can pass an argument
+            like "!infer" or "!train".
+    """
     def __init__(self,
                  fn: Callable,
                  inputs: Union[None, str, Iterable[str], Callable] = None,
