5d903738f3e3f621cbfb00cf3bbb02d44b251b41
geez0219 <geez0219@yahoo.com.tw>
2020-06-07 21:54:23 -0700

    fix issue of mutliple assertion

--- test/PR_test/unit_test/backend/test_gather_from_batch.py
+++ test/PR_test/unit_test/backend/test_gather_from_batch.py
@@ -28,25 +28,28 @@ class TestGatherFromBatch(unittest.TestCase):
         obj2 = tf.constant([[2, 3], [4, 5], [10, 11]])
         self.assertTrue(is_equal(obj1, obj2))
 
-    def test_gather_from_batch_torch_input(self):
+    def test_gather_from_batch_torch_input_2D(self):
         tensor = torch.tensor([[0, 1], [2, 3], [4, 5]])
         indice = torch.tensor([1, 0, 1])
         obj1 = fe.backend.gather_from_batch(tensor, indice)
         obj2 = torch.tensor([1, 2, 5])
         self.assertTrue(is_equal(obj1, obj2))
 
+    def test_gather_from_batch_torch_input_3D(self):
         tensor = torch.tensor([[[0, 1], [2, 3]], [[4, 5], [6, 7]], [[8, 9], [10, 11]]])
+        indice = torch.tensor([1, 0, 1])
         obj1 = fe.backend.gather_from_batch(tensor, indice)
         obj2 = torch.tensor([[2, 3], [4, 5], [10, 11]])
         self.assertTrue(is_equal(obj1, obj2))
 
-    def test_gather_from_batch_input_and_indice_diff_type(self):
+    def test_gather_from_batch_input_and_indice_diff_type_2D(self):
         tensor = torch.tensor([[0, 1], [2, 3], [4, 5]])
         indice = np.array([1, 0, 1])
         obj1 = fe.backend.gather_from_batch(tensor, indice)
         obj2 = torch.tensor([1, 2, 5])
         self.assertTrue(is_equal(obj1, obj2))
 
+    def test_gather_from_batch_input_and_indice_diff_type_3D(self):
         tensor = tf.constant([[[0, 1], [2, 3]], [[4, 5], [6, 7]], [[8, 9], [10, 11]]])
         indice = torch.tensor([1, 0, 1])
         obj1 = fe.backend.gather_from_batch(tensor, indice)
