c2797fcbda404fe9c0821dd1f0313bdc6a2b0d7e
Michael Potter <Michael.Potter@GE.com>
2020-06-10 17:44:19 -0700

    move schedulers forward

--- fastestimator/util/traceability_util.py
+++ fastestimator/util/traceability_util.py
@@ -909,15 +909,16 @@ def fe_summary(self) -> List[FeSummaryTable]:
     from fastestimator.pipeline import Pipeline
     from fastestimator.op.op import Op
     from fastestimator.trace.trace import Trace
+    from fastestimator.schedule.schedule import Scheduler
     from torch.utils.data import Dataset
     # re-number the references for nicer viewing
     ordered_items = sorted(
         self._fe_traceability_summary.items(),
         key=lambda x: 0 if issubclass(x[1].type, Estimator) else 1
         if issubclass(x[1].type, (TFNetwork, TorchNetwork)) else 2 if issubclass(x[1].type, Pipeline) else 3
-        if issubclass(x[1].type, Trace) else 4 if issubclass(x[1].type, Op) else 5
-        if issubclass(x[1].type, (Dataset, tf.data.Dataset)) else 6
-        if not issubclass(x[1].type, (np.ndarray, tf.Tensor, torch.Tensor)) else 7)
+        if issubclass(x[1].type, Scheduler) else 4 if issubclass(x[1].type, Trace) else 5
+        if issubclass(x[1].type, Op) else 6 if issubclass(x[1].type, (Dataset, tf.data.Dataset)) else 7
+        if not issubclass(x[1].type, (np.ndarray, tf.Tensor, torch.Tensor)) else 8)
     key_mapping = {fe_id: idx for idx, (fe_id, val) in enumerate(ordered_items)}
     FEID.set_translation_dict(key_mapping)
     return [item[1] for item in ordered_items]
