a8fc889297105608c999c720af5f1ce3f3933f81
Purujit Bahl <purujitb@gmail.com>
2020-06-12 17:30:03 -0700

    renamed the test methods

--- test/PR_test/unit_test/layers/tensorflow/test_instance_norm.py
+++ test/PR_test/unit_test/layers/tensorflow/test_instance_norm.py
@@ -7,7 +7,7 @@ import fastestimator as fe
 
 
 class TestInstanceNorm(unittest.TestCase):
-    def test_reflection_padding_2d_10(self):
+    def test_instance_norm(self):
         n = tfp.distributions.Normal(loc=10, scale=2)
         x = n.sample(sample_shape=(1, 100, 100, 1))
         m = fe.layers.tensorflow.InstanceNormalization()
