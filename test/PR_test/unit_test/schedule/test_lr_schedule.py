6a52a115b0d9f571a1a430f5610ae0f94970efe6
vbvg2008 <shawnmengdong@gmail.com>
2020-07-08 23:26:09 +0000

    extend our cosine decay to allow restart

--- test/PR_test/unit_test/schedule/test_lr_schedule.py
+++ test/PR_test/unit_test/schedule/test_lr_schedule.py
@@ -6,9 +6,17 @@ from fastestimator.schedule.lr_shedule import cosine_decay
 
 class TestLRSchedule(unittest.TestCase):
     def test_cosine_decay(self):
-        learning_rate = cosine_decay(time=5, cycle_length=10, init_lr=0.01, min_lr=0.0001)
-        self.assertTrue(math.isclose(learning_rate, 0.006579, rel_tol=1e-3))
+        learning_rate = cosine_decay(time=5, cycle_length=10, init_lr=0.01, min_lr=0.0)
+        self.assertEqual(learning_rate, 0.005)
 
     def test_cosine_decay_cycle(self):
-        learning_rate = cosine_decay(time=11, cycle_length=10, init_lr=0.01, min_lr=0.0001)
-        self.assertEqual(learning_rate, 0.01)
+        learning_rate = cosine_decay(time=1001, cycle_length=1000, init_lr=0.01, min_lr=0.0)
+        self.assertTrue(math.isclose(learning_rate, 0.01, rel_tol=1e-3))
+
+    def test_cosine_decay_multiplier_mid_cycle(self):
+        learning_rate = cosine_decay(5, cycle_length=10, init_lr=0.01, min_lr=0.0, cycle_multiplier=2)
+        self.assertEqual(learning_rate, 0.005)
+
+    def test_cosine_decay_multiplier_end_cycle(self):
+        learning_rate = cosine_decay(10, cycle_length=10, init_lr=0.01, min_lr=0.0, cycle_multiplier=2)
+        self.assertEqual(learning_rate, 0.0)
