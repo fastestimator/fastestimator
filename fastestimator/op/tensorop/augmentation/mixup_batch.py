f10db31557c0ac94d7eb75d86461e5ac33797b2b
Purujit Bahl <purujitb@gmail.com>
2020-07-15 15:25:52 -0700

    added mixup_batch tensorop

---
+++ fastestimator/op/tensorop/augmentation/mixup_batch.py
@@ -0,0 +1,72 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import tensorflow as tf
+import tensorflow_probability as tfp
+import torch
+
+from fastestimator.op.tensorop import TensorOp
+
+
+class MixUpBatch(TensorOp):
+    """ This class should be used in conjunction with MixUpLoss to perform mix-up training, which helps to reduce
+    over-fitting, stabilize GAN training, and against adversarial attacks (https://arxiv.org/abs/1710.09412)
+
+    Args:
+        inputs: key of the input to be mixed up
+        outputs: key to store the mixed-up input
+        mode: what mode to execute in. Probably 'train'
+        alpha: the alpha value defining the beta distribution to be drawn from during training
+    """
+    def __init__(self, inputs=None, outputs=None, mode=None, alpha=1.0):
+        assert alpha > 0, "Mixup alpha value must be greater than zero"
+        super().__init__(inputs=inputs, outputs=outputs, mode=mode)
+
+        self.first_iteration = True
+
+    def forward(self, data, state):
+        """ Forward method to perform mixup batch augmentation
+
+        Args:
+            data: Batch data to be augmented
+            state: Information about the current execution context.
+
+        Returns:
+            Mixed-up batch data
+        """
+        if self.first_iteration:
+            self.first_iteration = False
+            if tf.is_tensor(data):
+                self.alpha = tf.constant(self.alpha)
+                self.beta = tfp.distributions.Beta(self.alpha, self.alpha)
+            elif isinstance(data, torch.Tensor):
+                self.alpha = torch.FloatTensor(self.alpha)
+                self.beta = torch.distributions.beta.Beta(self.alpha, self.alpha)
+            else:
+                raise ValueError("unrecognized data format: {}".format(type(data)))
+
+        iterdata = data if isinstance(data, list) else list(data) if isinstance(data, tuple) else [data]
+        lam = self.beta.sample()
+        # Could do random mix-up using tf.gather() on a shuffled index list, but batches are already randomly ordered,
+        # so just need to roll by 1 to get a random combination of inputs. This also allows MixUpLoss to easily compute
+        # the corresponding Y values
+        if tf.is_tensor(data):
+            mix = [lam * dat + (1.0 - lam) * tf.roll(dat, shift=1, axis=0) for dat in iterdata]
+        elif isinstance(data, torch.Tensor):
+            mix = [lam * dat + (1.0 - lam) * torch.roll(dat, shift=1, dims=0) for dat in iterdata]
+        else:
+            raise ValueError("unrecognized data format: {}".format(type(data)))
+
+        return mix + [lam]
