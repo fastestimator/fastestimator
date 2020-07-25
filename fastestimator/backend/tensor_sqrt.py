8b7928b03209e1e2372f4192d43705727b8b85fc
Vivek <viveks2410@gmail.com>
2020-07-25 14:20:22 -0700

    docstring update in tensor_sqrt

--- fastestimator/backend/tensor_sqrt.py
+++ fastestimator/backend/tensor_sqrt.py
@@ -27,19 +27,19 @@ def tensor_sqrt(tensor: Tensor) -> Tensor:
     This method can be used with Numpy data:
     ```python
     n = np.array([[1, 4, 6], [4, 9, 16]])
-    b = fe.backend.sqrt(n)  # [[1.0, 2.0, 2.44948974], [2.0, 3.0, 4.0]]
+    b = fe.backend.tensor_sqrt(n)  # [[1.0, 2.0, 2.44948974], [2.0, 3.0, 4.0]]
     ```
 
     This method can be used with TensorFlow tensors:
     ```python
     t = tf.constant([[1, 4, 6], [4, 9, 16]], dtype=tf.float32)
-    b = fe.backend.sqrt(t)  # [[1.0, 2.0, 2.4494898], [2.0, 3.0, 4.0]]
+    b = fe.backend.tensor_sqrt(t)  # [[1.0, 2.0, 2.4494898], [2.0, 3.0, 4.0]]
     ```
 
     This method can be used with PyTorch tensors:
     ```python
     p = torch.tensor([[1, 4, 6], [4, 9, 16]], dtype=torch.float32)
-    b = fe.backend.sqrt(p)  # [[1.0, 2.0, 2.4495], [2.0, 3.0, 4.0]]
+    b = fe.backend.tensor_sqrt(p)  # [[1.0, 2.0, 2.4495], [2.0, 3.0, 4.0]]
     ```
 
     Args:
