3f98b95e962cd586914c39b1125d6828d1a26166
Purujit Bahl <purujitb@gmail.com>
2020-07-28 22:31:35 -0700

    added torch test too

--- test/PR_test/unit_test/op/loss/test_mix_loss.py
+++ test/PR_test/unit_test/op/loss/test_mix_loss.py
@@ -2,12 +2,13 @@ import unittest
 
 import tensorflow as tf
 import numpy as np
+import torch
 
 from fastestimator.op.tensorop.loss import MixLoss, CrossEntropy
 
 
 class TestMixUpBatch(unittest.TestCase):
-    def test_mixup_batch(self):
+    def test_mixup_batch_tf(self):
         true_binary = tf.constant([[1.0], [2.0], [4.0]])
         pred_binary = tf.constant([[1.0], [3.0], [4.5]])
 
@@ -15,3 +16,12 @@ class TestMixUpBatch(unittest.TestCase):
         output = ml.forward(data=[0.1, pred_binary, true_binary], state={})
 
         self.assertTrue(np.allclose(output[0].numpy(), -20.444319))
+
+    def test_mixup_batch_torch(self):
+        true_binary = torch.tensor([[1], [0], [1], [0]])
+        pred_binary = torch.tensor([[0.9], [0.3], [0.8], [0.1]])
+
+        ml = MixLoss(CrossEntropy(inputs=("y_pred", "y"), mode="train", outputs="loss"), lam="lambda")
+        output = ml.forward(data=[0.1, pred_binary, true_binary], state={})
+
+        self.assertTrue(np.allclose(output[0].detach().numpy(), 1.6889441))
