54c9254098aee89520814ed54b6e6016b821424f
Fei Tan <phillistan705@gmail.com>
2021-06-10 14:10:31 -0700

    Fix description of ExpandDims()

--- fastestimator/op/numpyop/univariate/expand_dims.py
+++ fastestimator/op/numpyop/univariate/expand_dims.py
@@ -22,7 +22,7 @@ from fastestimator.util.traceability_util import traceable
 
 @traceable()
 class ExpandDims(NumpyOp):
-    """Transpose the data (for example to make it channel-width-height instead of width-height-channel)
+    """Expand the dimension of inputs by inserting a new axis to the specified axis position 
 
     Args:
         inputs: Key(s) of inputs to be modified.
