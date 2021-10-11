84644b736f0a14a16b8de05d9ea4e55ede43b6d1
Vivek <viveks2410@gmail.com>
2021-10-11 21:39:16 +0000

    enable module override in per_ds for website parsing

--- fastestimator/trace/meta/per_ds.py
+++ fastestimator/trace/meta/per_ds.py
@@ -103,5 +103,6 @@ def per_ds(clz: type(Trace)):
 
     PerDS.__name__ = clz.__name__
     PerDS.__qualname__ = clz.__qualname__
-    # PerDS.__module__ = clz.__module__  # We don't override the module so that debugging is more clear
+    PerDS.__module__ = clz.__module__  
+    PerDS.__doc__ = clz.__doc__ # We want to preserve the docstring of the original class
     return PerDS
