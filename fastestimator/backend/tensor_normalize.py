fe8c227a618ae339c5ccd847e5729b590345efc6
Ashok <ashokaddala@gmail.com>
2022-01-13 10:57:18 -0600

    Adding unit tests

--- fastestimator/backend/tensor_normalize.py
+++ fastestimator/backend/tensor_normalize.py
@@ -1,80 +1,84 @@
-from typing import TypeVar
+from typing import Tuple, TypeVar
 
+import torch
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.keras.backend_config import epsilon
-import torch
+
 
 from fastestimator.backend.reduce_mean import reduce_mean
 from fastestimator.backend.reduce_std import reduce_std
-from fastestimator.backend import to_tensor
-
-Tensor = TypeVar('Tensor', tf.Tensor, torch.Tensor, np.ndarray)
-from typing import Sequence, TypeVar, Union
+from fastestimator.backend.to_tensor import to_tensor
 
 Tensor = TypeVar('Tensor', tf.Tensor, torch.Tensor, np.ndarray)
 
 
 def normalize(tensor: Tensor, mean, std, epsilon: float = 1e-7) -> Tensor:
-    """Compute the std value along a given `axis` of a `tensor`.
+    """Compute the normalized value of a `tensor`.
 
     This method can be used with Numpy data:
     ```python
     n = np.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-    b = fe.backend.reduce_std(n)  # 2.2913
-    b = fe.backend.reduce_std(n, axis=0)  # [[2., 2.], [2., 2.]]
-    b = fe.backend.reduce_std(n, axis=1)  # [[1., 1.], [1., 1.]]
-    b = fe.backend.reduce_std(n, axis=[0,2])  # [2.06155281, 2.06155281]
+    b = fe.backend.tensor_normalize(n, None, None)  # ([[[-1.52752516, -1.0910894 ], [-0.65465364, -0.21821788]], [[ 0.21821788,  0.65465364], [ 1.0910894 ,  1.52752516]]])
+    b = fe.backend.tensor_normalize(n, 4.5, 2.29128784747792)  # ([[[-1.52752516, -1.0910894 ], [-0.65465364, -0.21821788]], [[ 0.21821788,  0.65465364], [ 1.0910894 ,  1.52752516]]])
+    b = fe.backend.tensor_normalize(n, (4., 5.), (2.23606798, 2.23606798))  # [[[-1.34164073, -1.34164073], [-0.44721358, -0.44721358]], [[ 0.44721358,  0.44721358], [ 1.34164073,  1.34164073]]]
     ```
 
     This method can be used with TensorFlow tensors:
     ```python
     t = tf.constant([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-    b = fe.backend.reduce_std(t)  # 2.2913
-    b = fe.backend.reduce_std(t, axis=0)  # [[2., 2.], [2., 2.]]
-    b = fe.backend.reduce_std(t, axis=1)  # [[2, 3], [3, 7]]
-    b = fe.backend.reduce_std(t, axis=[0,2])  # [2.06155281, 2.06155281]
+    b = fe.backend.tensor_normalize(n, None, None)  # ([[[-1.52752516, -1.0910894 ], [-0.65465364, -0.21821788]], [[ 0.21821788,  0.65465364], [ 1.0910894 ,  1.52752516]]])
+    b = fe.backend.tensor_normalize(n, 4.5, 2.29128784747792)  # ([[[-1.52752516, -1.0910894 ], [-0.65465364, -0.21821788]], [[ 0.21821788,  0.65465364], [ 1.0910894 ,  1.52752516]]])
+    b = fe.backend.tensor_normalize(n, (4., 5.), (2.23606798, 2.23606798))  # [[[-1.34164073, -1.34164073], [-0.44721358, -0.44721358]], [[ 0.44721358,  0.44721358], [ 1.34164073,  1.34164073]]]
     ```
 
     This method can be used with PyTorch tensors:
     ```python
     p = torch.tensor([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
-    b = fe.backend.reduce_std(p)  # 2.2913
-    b = fe.backend.reduce_std(p, axis=0)  # [[2., 2.], [2., 2.]]
-    b = fe.backend.reduce_std(p, axis=1)  # [[1., 1.], [1., 1.]]
-    b = fe.backend.reduce_std(p, axis=[0,2])  # [2.06155281, 2.06155281]
+    b = fe.backend.tensor_normalize(n, None, None)  # ([[[-1.52752516, -1.0910894 ], [-0.65465364, -0.21821788]], [[ 0.21821788,  0.65465364], [ 1.0910894 ,  1.52752516]]])
+    b = fe.backend.tensor_normalize(n, 4.5, 2.29128784747792)  # ([[[-1.52752516, -1.0910894 ], [-0.65465364, -0.21821788]], [[ 0.21821788,  0.65465364], [ 1.0910894 ,  1.52752516]]])
+    b = fe.backend.tensor_normalize(n, (4., 5.), (2.23606798, 2.23606798))  # [[[-1.34164073, -1.34164073], [-0.44721358, -0.44721358]], [[ 0.44721358,  0.44721358], [ 1.34164073,  1.34164073]]]
     ```
 
     Args:
         tensor: The input value.
-        axis: Which axis or collection of axes to compute the std along.
-        keepdims: Whether to preserve the number of dimensions during the reduction.
+        mean: The mean which needs to applied(eg: None, 3.8, (1.9, 2.0, 2.9))
+        std: The standard deviation which needs to applied(eg: None, 3.8, (1.9, 2.0, 2.9))
 
     Returns:
-        The std values of `tensor` along `axis`.
+        The normalized values of `tensor`.
 
     Raises:
         ValueError: If `tensor` is an unacceptable data type.
     """
-    mean = get_mean(tensor, mean)
-    std = get_std(tensor, std)
+    channel_level = False
+    if isinstance(mean, Tuple) or isinstance(std, Tuple):
+        channel_level = True
+
+    mean = get_mean(tensor, mean, channel_level)
+    std = get_std(tensor, std, channel_level)
+    epsilon = get_epsilon(tensor, epsilon)
 
     if tf.is_tensor(tensor) or isinstance(tensor, torch.Tensor) or isinstance(tensor, np.ndarray):
-        tensor = (tensor - mean) / std
-        if isinstance(tensor, torch.Tensor):
-            tensor = tensor.permute((0, 3, 1, 2))  # channel first
+        tensor = (tensor - mean) / (std + epsilon)
+
+        # for testing purpose we are not changing the channels 
+        #if isinstance(tensor, torch.Tensor):
+        #    tensor = tensor.permute((0, 1, 2, 3))  # channel first
     else:
         raise ValueError("Unrecognized tensor type {}".format(type(tensor)))
 
     return tensor
 
 
-def get_mean(tensor: Tensor, mean):
+def get_mean(tensor: Tensor, mean, channel_level):
     if mean == None:
-        return reduce_mean(tensor)
+        if channel_level:
+            return reduce_mean(tensor, axis=list(range(0, len(tensor.shape)-1)))
+        else:
+            return reduce_mean(tensor)
 
     if tf.is_tensor(tensor):
-        mean = tf.convert_to_tensor(mean)
+        mean = tf.cast(tf.convert_to_tensor(mean), tf.float32)
     elif isinstance(tensor, torch.Tensor):
         mean = to_tensor(mean, "torch").type(torch.float32)
     elif isinstance(tensor, np.ndarray):
@@ -85,13 +89,28 @@ def get_mean(tensor: Tensor, mean):
     return mean
 
 
-def get_std(tensor: Tensor, std):
-
-    if std == None:
-        return reduce_std(tensor)
+def get_epsilon(tensor: Tensor, epsilon):
 
     if tf.is_tensor(tensor):
-        std = tf.convert_to_tensor(std)
+        return tf.cast(tf.convert_to_tensor(epsilon), tf.float32)
+    elif isinstance(tensor, torch.Tensor):
+        return to_tensor(epsilon, "torch").type(torch.float32)
+    elif isinstance(tensor, np.ndarray):
+        return epsilon
+    else:
+        raise ValueError("Unrecognized tensor type {}".format(type(tensor)))
+
+
+def get_std(tensor: Tensor, std, channel_level):
+
+    if std == None:
+        if channel_level:
+            return reduce_std(tensor, axis=list(range(0, len(tensor.shape)-1)))
+        else:
+            return reduce_std(tensor)
+
+    if tf.is_tensor(tensor):
+        std = tf.cast(tf.convert_to_tensor(std), tf.float32)
     elif isinstance(tensor, torch.Tensor):
         std = to_tensor(std, "torch").type(torch.float32)
     elif isinstance(tensor, np.ndarray):
---
+++ test/PR_test/unit_test/backend/test_tensor_normalize.py
@@ -0,0 +1,40 @@
+# Copyright 2020 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import unittest
+
+import numpy as np
+import tensorflow as tf
+import torch
+
+from fastestimator.backend.tensor_normalize import normalize
+
+
+class TestNormalize(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.test_np = np.array([[1, 2], [3, 3]])
+        self.test_tf = tf.cast(tf.constant([[1, 1], [2, -3], [4, 1]]), tf.float32)
+        self.test_torch = torch.Tensor([[1, 1], [2, -3], [4, 1]])
+
+    def test_normalize_np_value(self):
+        np.testing.assert_array_almost_equal(normalize(self.test_np, None, None), np.array([[-1.50755654, -0.30151131], [ 0.90453392,  0.90453392]]))
+
+    def test_normalize_tf_value(self):
+        np.testing.assert_array_almost_equal(normalize(self.test_tf, None, None).numpy(), np.array([[ 0. ,  0.], [ 0.48038446, -1.92153785], [ 1.44115338,  0.]]))
+
+    def test_normalize_torch_value(self):
+        print(normalize(self.test_torch, None, None))
+        #np.testing.assert_array_almost_equal(normalize(self.test_torch, None, None).numpy(), np.array([[ 0. ,  0.], [ 0.48038446, -1.92153785], [ 1.44115338,  0.]]))
+
