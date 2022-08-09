c1fd1d13e912f14f9c771abd4ce4648c5ab71e89
Michael Potter <NanoTekGuy@Gmail.com>
2022-08-09 14:25:31 -0700

    Add filtering functionality to the csv dataset class

--- fastestimator/dataset/csv_dataset.py
+++ fastestimator/dataset/csv_dataset.py
@@ -12,11 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import inspect
+import math
 import os
+from typing import Any, Callable, Dict, Iterable, Optional, Union
 
 import pandas as pd
 
 from fastestimator.dataset.dataset import InMemoryDataset
+from fastestimator.util.base_util import to_list
 from fastestimator.util.traceability_util import traceable
 
 
@@ -31,10 +35,45 @@ class CSVDataset(InMemoryDataset):
     Args:
         file_path: The (absolute) path to the CSV file.
         delimiter: What delimiter is used by the file.
+        include_if: An optional filter specifying which rows should be included. This can be a dictionary, for example
+            {'mode': 'train', 'type': [0, 1, 2]} in which case only rows which have a 'mode' column value of 'train' AND
+            a 'type' column value of either 0, 1, or 2 will be included in this dataset. Alternatively, this can be a
+            query string: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#the-query-method, for
+            example 'type >= 1'. Finally, it could be a function whose argument(s) correspond to column names and whose
+            output is a boolean (include_if=lambda mode: mode in ['eval', 'test']). This last option is very flexible,
+            but also slower to execute.
+        fill_na: A fill value if data is missing. By default, this will follow pandas convention and use different types
+            of NaNs.
         kwargs: Other arguments to be passed through to pandas csv reader function. See the pandas docs for details:
             https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html.
     """
-    def __init__(self, file_path: str, delimiter: str = ",", **kwargs) -> None:
+
+    def __init__(self,
+                 file_path: str,
+                 delimiter: str = ",",
+                 include_if: Union[None, Dict[str, Union[Any, Iterable[Any]]], str, Callable[..., bool]] = None,
+                 fill_na: Optional[Any] = 'pandas_default',
+                 **kwargs) -> None:
         df = pd.read_csv(file_path, delimiter=delimiter, **kwargs)
+        if fill_na is None:
+            df = df.fillna(math.nan).replace([math.nan], [None])
+        elif fill_na != 'pandas_default':
+            df = df.fillna(value=fill_na)
+        if include_if is not None:
+            if isinstance(include_if, dict):
+                for k, v in include_if.items():
+                    v = [None] if v is None else to_list(v)
+                    df = df[df[k].isin(v)]
+            elif isinstance(include_if, str):
+                df = df.query(include_if)
+            elif hasattr(include_if, "__call__"):
+                cols = list(inspect.signature(include_if).parameters.keys())
+                for col in cols:
+                    if col not in df.columns:
+                        raise ValueError(f"The provided filter function requested '{col}' which was not found in the "
+                                         f"csv columns: {list(df.columns)}")
+                df = df[df[cols].apply(lambda row: include_if(**row), axis=1)]
+            else:
+                raise ValueError(f"Received an unexpected datatype for include_if: {type(include_if)}")
         self.parent_path = os.path.dirname(file_path)
         super().__init__(df.to_dict(orient='index'))
