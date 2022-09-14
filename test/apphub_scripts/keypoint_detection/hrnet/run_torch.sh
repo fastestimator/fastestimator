b817ac312104c144f51d38e484dc7428ad7c7803
vbvg2008 <shawnmengdong@gmail.com>
2022-09-14 13:47:05 -0700

    update copyright notice

--- test/apphub_scripts/keypoint_detection/hrnet/run_torch.sh
+++ test/apphub_scripts/keypoint_detection/hrnet/run_torch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 The FastEstimator Authors. All Rights Reserved.
+# Copyright 2022 The FastEstimator Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
