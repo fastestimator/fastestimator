489b5df6a3d2ffd0721d5e5b0e803f089abf0676
Ashok Vardhan Addala <ashokaddala@gmail.com>
2022-10-07 13:25:45 -0700

    moving cifar dataset (#1326)

--- fastestimator/dataset/data/cifar100.py
+++ fastestimator/dataset/data/cifar100.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+# Copyright 2022 The FastEstimator Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,24 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import os
+import tarfile
+from pathlib import Path
 from typing import Tuple
 
-import tensorflow as tf
+import numpy as np
+from keras.datasets.cifar import load_batch
 
 from fastestimator.dataset.numpy_dataset import NumpyDataset
+from fastestimator.util.google_download_util import _download_file_from_google_drive
 
 
-def load_data(image_key: str = "x",
+def load_data(root_dir: str = None,
+              image_key: str = "x",
               label_key: str = "y",
-              label_mode: str = "fine") -> Tuple[NumpyDataset, NumpyDataset]:
+              label_mode: str = "fine",
+              channels_last: bool = True) -> Tuple[NumpyDataset, NumpyDataset]:
     """Load and return the CIFAR100 dataset.
 
     Please consider using the ciFAIR100 dataset instead. CIFAR100 contains duplicates between its train and test sets.
 
     Args:
+        root_dir: The path to store the downloaded data. When `path` is not provided, the data will be saved into
+            `fastestimator_data` under the user's home directory.
         image_key: The key for image.
         label_key: The key for label.
         label_mode: Either "fine" for 100 classes or "coarse" for 20 classes.
+        channels_last: Whether channel is last
 
     Returns:
         (train_data, eval_data)
@@ -40,7 +50,39 @@ def load_data(image_key: str = "x",
     print("\033[93m {}\033[00m".format("FastEstimator-Warn: Consider using the ciFAIR100 dataset instead."))
     if label_mode not in ['fine', 'coarse']:
         raise ValueError("label_mode must be one of either 'fine' or 'coarse'.")
-    (x_train, y_train), (x_eval, y_eval) = tf.keras.datasets.cifar100.load_data(label_mode=label_mode)
+    home = str(Path.home())
+
+    if root_dir is None:
+        root_dir = os.path.join(home, 'fastestimator_data', 'cifar100')
+    else:
+        root_dir = os.path.join(os.path.abspath(root_dir), 'cifar100')
+    os.makedirs(root_dir, exist_ok=True)
+
+    image_compressed_path = os.path.join(root_dir, 'cifar100.tar.gz')
+    image_extracted_path = os.path.join(root_dir, 'cifar-100-python')
+
+    if not os.path.exists(image_extracted_path):
+        if not os.path.exists(image_compressed_path):
+            print("Downloading data to {}".format(root_dir))
+            _download_file_from_google_drive('1ntXqOaXMaq4TcvpCaOCpqqNCjYy2oVsb', image_compressed_path)
+
+        print("Extracting data to {}".format(root_dir))
+        with tarfile.open(image_compressed_path) as img_tar:
+            img_tar.extractall(root_dir)
+
+    train_data_path = os.path.join(image_extracted_path, "train")
+    x_train, y_train = load_batch(train_data_path, label_key=label_mode + "_labels")
+
+    eval_data_path = os.path.join(image_extracted_path, "test")
+    x_eval, y_eval = load_batch(eval_data_path, label_key=label_mode + "_labels")
+
+    y_train = np.expand_dims(y_train, -1)
+    y_eval = np.expand_dims(y_eval, -1)
+
+    if channels_last:
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_eval = x_eval.transpose(0, 2, 3, 1)
+
     train_data = NumpyDataset({image_key: x_train, label_key: y_train})
     eval_data = NumpyDataset({image_key: x_eval, label_key: y_eval})
     return train_data, eval_data
