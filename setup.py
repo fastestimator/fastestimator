cbf1663f088c8345b3f25a040a78840b04ab66a1
XD <shawnmengdong@gmail.com>
2023-04-25 13:39:51 -0700

    update uncertainty calibration 1.5.2 release (#1377)

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def get_dependency():
         'dot2tex==2.11.3',
         'gdown==3.12.0',
         'PySocks==1.7.1',
-        'uncertainty-calibration==0.0.9',
+        'uncertainty-calibration==0.1.4',
         'dill==0.3.4',
         'scikit-image==0.19.1',
         'prettytable==3.1.0',
