f0efccd60be0675ef60fc8980d98dda93f6adc9b
vbvg2008 <shawnmengdong@gmail.com>
2023-11-01 17:09:50 -0700

    fix nightly for some multi-gpu issues

--- apphub/foundation_model/lora/lora_torch.py
+++ apphub/foundation_model/lora/lora_torch.py
@@ -494,7 +494,10 @@ class BestLoRASaver(BestModelSaver):
             self.best = data[self.metric]
             self.since_best = 0
             if self.save_dir:
-                self.model_path = self.model.save_lora_weights(self.save_dir, self.model_name + "_lora")
+                if isinstance(self.model, torch.nn.DataParallel):
+                    self.model_path = self.model.module.save_lora_weights(self.save_dir, self.model_name + "_lora")
+                else:
+                    self.model_path = self.model.save_lora_weights(self.save_dir, self.model_name + "_lora")
         else:
             self.since_best += 1
         data.write_with_log(self.outputs[0], self.since_best)
