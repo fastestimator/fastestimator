6acdd182fb0ec9d1eb7f8eabc23fe180c79dcbb1
DwijayDS <dwijay1@gmail.com>
2023-11-13 11:14:16 -0800

    cyclegan multigpu padding fixed

--- test/apphub_scripts/image_generation/cyclegan/run_tf.sh
+++ test/apphub_scripts/image_generation/cyclegan/run_tf.sh
@@ -22,7 +22,7 @@ example_name="cyclegan"
 # 1. Usually we set the epochs:2, batch_size:2, train_steps_per_epoch:2
 # 2. The expression for the following setup is "--epochs 2 --batch_size 8 --train_steps_per_epoch 2"
 # 3. The syntax of this expression is different from run_notebook.py
-train_info=(--epochs 2 --batch_size 2 --train_steps_per_epoch 2)
+train_info=(--epochs 2 --batch_size_per_device 1 --train_steps_per_epoch 2)
 
 # Do you want to run "fastestimator test"? (bool)
 need_test=0
