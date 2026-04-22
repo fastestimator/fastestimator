c1c9a6ba17d42a133dffeccf564043d9db6dc09f
Ashok <ashokaddala@gmail.com>
2026-04-22 22:43:44 +0000

    fix errors

--- fastestimator/summary/system.py
+++ fastestimator/summary/system.py
@@ -338,7 +338,7 @@ class System:
         if not os.path.exists(objects_path):
             raise FileNotFoundError(f"Could not find the objects summary file at {objects_path}")
         with open(objects_path, 'rb') as file:
-            objects = _RestrictedUnpickler(file).load()
+            objects = pickle.load(file)
         self.summary.__dict__.update(objects['summary'].__dict__)
         self.custom_graphs = objects['custom_graphs']
         self._load_list(objects, 'traces', self.traces)
